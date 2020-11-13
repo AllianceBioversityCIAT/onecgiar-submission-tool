ce32a08206749c8ca2d69ee80ca3dd5181f0e430
Duque <J.Duque@cgiar.org>
2020-11-13 16:21:26 -0500

    user routes are created

---
+++ one-cgiar-back/src/routes/User.ts
@@ -0,0 +1,23 @@
+import { Router } from 'express'
+import { getUsers, createUsers, getUser, updateUser, deleteUser } from '../controllers/User'
+import { checkJwt } from '../middlewares/jwt'
+import { checkRole } from '../middlewares/role'
+
+const router = Router()
+
+// get all users
+router.get('/users', [checkJwt, checkRole(['admin'])], getUsers);
+
+// create an user
+router.post('/users', [checkJwt, checkRole(['admin'])], createUsers);
+
+// get user by id
+router.get('/users/:id', [checkJwt, checkRole(['admin'])], getUser);
+
+// update user
+router.put('/users/:id', [checkJwt, checkRole(['admin'])], updateUser);
+
+// delete user
+router.delete('/users/:id',[checkJwt, checkRole(['admin'])], deleteUser);
+
+export default router;
