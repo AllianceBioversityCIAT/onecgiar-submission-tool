59920688079868ad9edcc9d00d4128a4ebc15a9b
Duque <J.Duque@cgiar.org>
2020-11-30 14:55:49 -0500

    role middleware is created

--- one-cgiar-back/src/middlewares/role.ts
+++ one-cgiar-back/src/middlewares/role.ts
@@ -2,22 +2,24 @@ import { Request, Response, NextFunction } from 'express'
 import { getRepository } from 'typeorm'
 import { User } from '../entity/User'
 
-export const checkRole = (roles:Array<string>) => {
+export const checkRole = (roles: Array<string>) => {
     return async (req: Request, res: Response, next: NextFunction) => {
         const { userId } = res.locals.jwtPayload;
         const userRepository = getRepository(User);
         let user: User;
 
         try {
-            user = await userRepository.findOneOrFail(userId);
+            user = await userRepository.findOneOrFail(userId, { relations: ['roles'] });
         } catch (error) {
             console.log(error);
             return res.status(401).json({ msg: 'No authorized' });
         }
 
         // check
-        const { role } = user;
-        if (roles.includes(role)) {
+        const checked = roles.some(role => {
+            return user.roles.some(userRole => userRole.acronym === role)
+        })
+        if (checked) {
             next();
         } else {
             res.status(401).json({ msg: 'No authorized' });
