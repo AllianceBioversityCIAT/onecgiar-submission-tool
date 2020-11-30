7d5dde92f5d65fd375b390815e661da4a42feb20
Duque <J.Duque@cgiar.org>
2020-11-30 15:00:06 -0500

    the way to use the role middleware is modified in the user routes

--- one-cgiar-back/src/routes/User.ts
+++ one-cgiar-back/src/routes/User.ts
@@ -2,22 +2,23 @@ import { Router } from 'express'
 import { getUsers, createUsers, getUser, updateUser, deleteUser } from '../controllers/User'
 import { checkJwt } from '../middlewares/jwt'
 import { checkRole } from '../middlewares/role'
+import { RolesHandler } from '../helpers/RolesHandler'
 
 const router = Router()
 
 // get all users
-router.get('/users', [checkJwt, checkRole(['admin'])], getUsers);
+router.get('/users', [checkJwt,checkRole([RolesHandler.admin])], getUsers);
 
 // create an user
-router.post('/users', [checkJwt, checkRole(['admin'])], createUsers);
+router.post('/users', [checkJwt,checkRole([RolesHandler.admin])], createUsers);
 
 // get user by id
-router.get('/users/:id', [checkJwt, checkRole(['admin'])], getUser);
+router.get('/users/:id', [checkJwt,checkRole([RolesHandler.admin])], getUser);
 
 // update user
-router.put('/users/:id', [checkJwt, checkRole(['admin'])], updateUser);
+router.put('/users/:id', [checkJwt,checkRole([RolesHandler.admin])], updateUser);
 
 // delete user
-router.delete('/users/:id',[checkJwt, checkRole(['admin'])], deleteUser);
+router.delete('/users/:id', [checkJwt,checkRole([RolesHandler.admin])], deleteUser);
 
 export default router;
