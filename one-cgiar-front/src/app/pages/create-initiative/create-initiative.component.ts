c8b5c17b13a1f2e320e5f1ac48591f427676734e
Jaime Duque <j.duque@cgiar.org>
2020-12-14 15:21:39 -0500

    manage access component is implemented

--- one-cgiar-front/src/app/pages/create-initiative/create-initiative.component.ts
+++ one-cgiar-front/src/app/pages/create-initiative/create-initiative.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { CoordinatorModalComponent } from '@app/shared/components/coordinator-modal/coordinator-modal.component';
 import { AuthService } from '../auth/auth.service';
 
 @Component({
@@ -8,13 +10,21 @@ import { AuthService } from '../auth/auth.service';
 })
 export class CreateInitiativeComponent implements OnInit {
 
-  constructor(public _auth:AuthService) { }
+  constructor(public _auth: AuthService, public dialog: MatDialog) { }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(CoordinatorModalComponent, { panelClass: 'custom-dialog-container' });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 
   ngOnInit(): void {
   }
 
   onSave(generalInformationForm): void {
-    console.log("GUARDANDO",generalInformationForm.value);
+    console.log("GUARDANDO", generalInformationForm.value);
   }
 
 }
