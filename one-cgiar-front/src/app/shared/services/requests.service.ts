d545373278488e406c990a0618ca7895f0945f67
Jaime Duque <j.duque@cgiar.org>
2021-01-20 15:51:40 -0500

    service for projection of benefits component is modified

--- one-cgiar-front/src/app/shared/services/requests.service.ts
+++ one-cgiar-front/src/app/shared/services/requests.service.ts
@@ -31,119 +31,261 @@ export class RequestsService {
     description: new FormControl('', Validators.required),
     results: new FormControl('', Validators.required),
   });
-
-    constructor() { }
-
-urlId;
-
-addedCoordinator: User[] = [
-  { id: 1, name: 'Jaime Duque', email: 'J.Duque@cgiar.org' }
-];
-
-coordinatorList: User[] = [
-  { id: 2, name: 'Manuel Almanzar', email: 'M.R.Almanzar@cgiar.org' },
-  { id: 3, name: 'Yecksin Zuñiga', email: 'Y.Zuniga@cgiar.org' },
-  { id: 4, name: 'Felipe Elvira', email: 'F.Elvira@cgiar.org' }
-];
-
-workPackages = [
-  {
-    name: 'Work package 1',
-    id: 1
-  }
-];
-
-contributions = [
-  {
-    id: 1
-  }
-];
-
-ranges = [
-  {
-    id: 1,
-    low: null,
-    high: null
-  }
-];
-
-addWorkPackage() {
-  let data = {
-    name: '',
-    id: 999
-  }
-  data.name = `Work package ${this.workPackages.length + 1}`;
-  data.id = this.workPackages.length + 1;
-  this.workPackages.push(data);
-  // this.workPackages.push(`Work package ${this.workPackages.length + 1}`);
-}
-
-removeWorkPackage() {
-  this.workPackages.splice(this.urlId - 1);
-  // this.workPackages.forEach((value, index) => {
-  //   if (value.id == workPackage.id) this.workPackages.splice(index, 1);
-  // });
-}
-
-addCoordinator(coordinator: User) {
-  console.log('coordinator', coordinator);
-  this.addedCoordinator.push(coordinator);
-}
-
-removeCoordinator(coordinator: User) {
-  this.addedCoordinator.forEach((value, index) => {
-    if (value.id == coordinator.id) this.addedCoordinator.splice(index, 1);
+  projectionBenefitsRangeCs = new FormGroup({
+    range: new FormArray([
+      new FormGroup({
+        year: new FormControl('2020'),
+        low: new FormControl('1'),
+        high: new FormControl('2'),
+      }),
+      new FormGroup({
+        year: new FormControl('2021'),
+        low: new FormControl('3'),
+        high: new FormControl('4'),
+      })
+    ]),
   });
-}
 
-addContribution() {
-  let contributionData = {
-    id: 999
+  constructor() { }
+
+  urlId;
+
+  addedCoordinator: User[] = [
+    { id: 1, name: 'Jaime Duque', email: 'J.Duque@cgiar.org' }
+  ];
+
+  coordinatorList: User[] = [
+    { id: 2, name: 'Manuel Almanzar', email: 'M.R.Almanzar@cgiar.org' },
+    { id: 3, name: 'Yecksin Zuñiga', email: 'Y.Zuniga@cgiar.org' },
+    { id: 4, name: 'Felipe Elvira', email: 'F.Elvira@cgiar.org' }
+  ];
+
+  workPackages = [
+    {
+      name: 'Work package 1',
+      id: 1
+    }
+  ];
+
+  contributions = [
+    {
+      id: 1
+    }
+  ];
+
+  ranges = [
+    {
+      id: 1,
+      low: null,
+      high: null
+    }
+  ];
+
+  impactAreas = [
+    {
+      id: 1,
+      name: "Nutrition, health & food security",
+      indicators: [
+        {
+          id: 1.1,
+          name: "#people benefiting from relevant CGIAR innovations"
+        },
+        {
+          id: 1.2,
+          name: "#people meeting minimum dietary energy requirements"
+        },
+        {
+          id: 1.3,
+          name: "#people meeting minimum micronutrient requirements"
+        },
+        {
+          id: 1.4,
+          name: "#cases communicable and noncommunicable diseases"
+        }
+      ]
+    },
+    {
+      id: 2,
+      name: "Poverty reduction, livelihoods & jobs",
+      indicators: [
+        {
+          id: 2.1,
+          name: "#people benefiting from relevant CGIAR innovations"
+        },
+        {
+          id: 2.2,
+          name: "#people assisted to exit poverty"
+        }
+      ]
+    },
+    {
+      id: 3,
+      name: "Gender equality, youth & social inclusion",
+      indicators: [
+        {
+          id: 3.1,
+          name: "#women’s empowerment and inclusion in the agricultural sector"
+        },
+        {
+          id: 3.2,
+          name: "#women benefiting from relevant CGIAR innovations"
+        },
+        {
+          id: 3.3,
+          name: "##youth benefiting fromrelevant CGIAR innovations"
+        },
+        {
+          id: 3.4,
+          name: "#women assisted to exit poverty"
+        }
+      ]
+    },
+    {
+      id: 4,
+      name: "Climate adaptation & mitigation",
+      indicators: [
+        {
+          id: 4.1,
+          name: "#tonnes CO2 equivalent emissions"
+        },
+        {
+          id: 4.2,
+          name: "#plans with evidence of implementation"
+        },
+        {
+          id: 4.3,
+          name: "#climate adaptation investments"
+        },
+        {
+          id: 4.4,
+          name: "#people benefiting from climate-adapted innovations"
+        }
+      ]
+    },
+    {
+      id: 5,
+      name: "Environmental health & biodiversity",
+      indicators: [
+        {
+          id: 5.1,
+          name: "#ha under improved management"
+        },
+        {
+          id: 5.2,
+          name: "#km3 consumptive water use"
+        },
+        {
+          id: 5.3,
+          name: "#ha deforestation"
+        },
+        {
+          id: 5.4,
+          name: "#Tg nitrogen application"
+        },
+        {
+          id: 5.5,
+          name: "#plant genetic accessions available and safely duplicated"
+        }
+      ]
+    },
+  ];
+
+  addWorkPackage() {
+    let data = {
+      name: '',
+      id: 999
+    }
+    data.name = `Work package ${this.workPackages.length + 1}`;
+    data.id = this.workPackages.length + 1;
+    this.workPackages.push(data);
+    // this.workPackages.push(`Work package ${this.workPackages.length + 1}`);
   }
-  contributionData.id = this.contributions.length + 1;
-  this.contributions.push(contributionData);
-}
 
-removeContribution(contribution: any) {
-  // this.contributions = this.contributions.filter(({ id }) => id !== contribution.id);
-  this.contributions.forEach((value, index) => {
-    if (value.id == contribution.id) this.contributions.splice(index, 1);
-  });
-}
-
-addRangeProjection() {
-  let rangeData = {
-    id: 2,
-    low: null,
-    high: null
+  removeWorkPackage() {
+    this.workPackages.splice(this.urlId - 1);
+    // this.workPackages.forEach((value, index) => {
+    //   if (value.id == workPackage.id) this.workPackages.splice(index, 1);
+    // });
   }
-  rangeData.id = this.ranges.length + 1;
-  this.ranges.push(rangeData);
-}
 
-removeRangeProjection(range: any) {
-  this.ranges.forEach((value, index) => {
-    if (value.id == range.id) this.ranges.splice(index, 1);
-  });
-}
+  addCoordinator(coordinator: User) {
+    console.log('coordinator', coordinator);
+    this.addedCoordinator.push(coordinator);
+  }
 
-saveGeneralInformation(): void {
-  console.log('formulario guardado', this.generalInformationFormCs);
-}
+  removeCoordinator(coordinator: User) {
+    this.addedCoordinator.forEach((value, index) => {
+      if (value.id == coordinator.id) this.addedCoordinator.splice(index, 1);
+    });
+  }
 
-saveNarratives(): void {
-  console.log('formulario guardado', this.narrativesFormCs);
-}
+  addContribution() {
+    let contributionData = {
+      id: 999
+    }
+    contributionData.id = this.contributions.length + 1;
+    this.contributions.push(contributionData);
+  }
 
-saveTheoryOfChange(): void {
-  console.log('formulario guardado', this.theoryOfChangeFormCs);
-}
+  removeContribution(contribution: any) {
+    // this.contributions = this.contributions.filter(({ id }) => id !== contribution.id);
+    this.contributions.forEach((value, index) => {
+      if (value.id == contribution.id) this.contributions.splice(index, 1);
+    });
+  }
 
-saveWorkPackageInformation(): void {
-  console.log('formulario guardado', this.workPackageInformationCs);
-}
+  addRangeProjection() {
+    let rangeData = {
+      id: 2,
+      low: null,
+      high: null
+    }
+    rangeData.id = this.ranges.length + 1;
+    this.ranges.push(rangeData);
+  }
 
-submitForm(): void {
-  console.log('formulario sometido', this.generalInformationFormCs);
-}
+  // removeRangeProjection(range: any) {
+  //   this.ranges.forEach((value, index) => {
+  //     if (value.id == range.id) this.ranges.splice(index, 1);
+  //   });
+  // }
+
+  removeRangeProjection(i) {
+    this.range.removeAt(i);
+  }
+
+  range = this.projectionBenefitsRangeCs.get("range") as FormArray;
+
+  addRange() {
+    const control = new FormGroup({
+      year: new FormControl(''),
+      low: new FormControl(''),
+      high: new FormControl(''),
+    });
+    this.range.push(control);
+  }
+
+  saveGeneralInformation(): void {
+    console.log('formulario guardado', this.generalInformationFormCs);
+  }
+
+  saveNarratives(): void {
+    console.log('formulario guardado', this.narrativesFormCs);
+  }
+
+  saveTheoryOfChange(): void {
+    console.log('formulario guardado', this.theoryOfChangeFormCs);
+  }
+
+  saveWorkPackageInformation(): void {
+    console.log('formulario guardado', this.workPackageInformationCs);
+  }
+
+  submitForm(): void {
+    console.log('formulario sometido', this.generalInformationFormCs);
+  }
+
+  saveIndicators(): void {
+    console.log('formulario de la gran puta', this.projectionBenefitsRangeCs)
+  }
 }
