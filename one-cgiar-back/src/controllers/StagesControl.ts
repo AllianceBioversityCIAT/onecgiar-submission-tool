173b89f348589dcc5383d507f5627a3cce113220
felipelvira <felipe.elvira.garcia@outlook.com>
2021-02-02 07:51:52 -0500

    Initiative concept: integration process updated

--- one-cgiar-back/src/controllers/StagesControl.ts
+++ one-cgiar-back/src/controllers/StagesControl.ts
@@ -1,8 +1,15 @@
 import { validate } from 'class-validator';
 import { Request, Response } from 'express'
-import { getRepository, In } from 'typeorm'
+import { getConnection, getRepository, In } from 'typeorm'
 import { ConceptInfo } from '../entity/ConceptInfo';
 import { InitiativesByStages } from '../entity/InititativesByStages';
+
+/****
+ * 
+ * **
+ *  CONCEPT
+ * **
+ */
 
 export const createConcept = async (req: Request, res: Response) => {
     const { name, challenge, objectives, results, highlights, action_area_id, action_area_description, initiative_by_stage_id } = req.body;
@@ -36,4 +43,63 @@ export const createConcept = async (req: Request, res: Response) => {
         console.log(error);
         res.status(404).json({ msg: "Could not create concept info." });
     }
+}
+
+/**
+ * 
+ * @param req params:{ stageId, initiativeId }
+ * @param res 
+ */
+
+export const getInitiativeConcept = async (req: Request, res: Response) => {
+    const { userId } = res.locals.jwtPayload;
+    const { stageId, initiativeId } = req.params;
+    const queryRunner = getConnection().createQueryBuilder();
+
+
+    let conceptInfo,
+        conceptQuery = ` 
+            SELECT
+                initvStg.id AS initvStgId,
+                stage.description AS stageDesc,
+                stage.active AS stageIsActive,
+                concept.id AS conceptInfoId,
+                concept.name AS conceptName,
+                concept.challenge AS conceptChallenge,
+                concept.objectives AS conceptObjectives,
+                concept.results AS conceptResults,
+                concept.highlights AS conceptHighlights,
+                concept.action_area_description AS conceptActAreDes,
+                concept.action_area_id AS conceptActAreId
+            FROM
+                initiatives_by_stages initvStg
+            LEFT JOIN stages stage ON stage.id = initvStg.initiativeId
+            LEFT JOIN concept_info concept ON concept.initvStgId = initvStgId
+            LEFT JOIN initiatives_by_users initvUsr ON initvUsr.initiativeId = initvStg.initiativeId
+
+            WHERE stage.id =:stageId
+            AND initvStg.initiativeId =:initiativeId
+            AND initvUsr.userId =:userId;
+        `;
+
+    try {
+        const [query, parameters] = await queryRunner.connection.driver.escapeQueryWithParameters(
+            conceptQuery,
+            { stageId, initiativeId, userId },
+            {}
+        );
+        conceptInfo = await queryRunner.connection.query(query, parameters);
+        console.log(query, parameters, userId)
+        if(conceptInfo.length == 0)
+            res.status(304).json({ msg: 'Concept info for empty', data: conceptInfo });
+
+        res.json({ msg: 'Concept info for stage', data: conceptInfo });
+    } catch (error) {
+        console.log(error);
+        res.status(404).json({ msg: "Could not get concept info." });
+    }
+
+
+
+
 }
