a63f45a2778d92ec50406d667460dfc4f8fde3ad
Jaime Duque <j.duque@cgiar.org>
2021-02-15 11:49:29 -0500

    narratives component is modified

--- one-cgiar-front/src/app/shared/components/concept/narratives-concept/narratives-concept.component.ts
+++ one-cgiar-front/src/app/shared/components/concept/narratives-concept/narratives-concept.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { InitiativesService } from '@app/shared/services/initiatives.service';
 import { RequestsService } from '@app/shared/services/requests.service';
 
 @Component({
@@ -7,6 +10,9 @@ import { RequestsService } from '@app/shared/services/requests.service';
   styleUrls: ['./narratives-concept.component.scss']
 })
 export class NarrativesConceptComponent implements OnInit {
+
+  public generalInformationForm: FormGroup;
+  public initvStgId: any;
 
   wordCount: any;
 
@@ -17,13 +23,56 @@ export class NarrativesConceptComponent implements OnInit {
     this.words = this.wordCount ? this.wordCount.length : 0;
   }
 
-  constructor(public _requests: RequestsService) { }
-
-  ngOnInit(): void {
+  constructor(public _requests: RequestsService, public initiativesSvc: InitiativesService, public activatedRoute: ActivatedRoute) {
+    this.generalInformationForm = new FormGroup({
+      name: new FormControl('', Validators.required),
+      challenge: new FormControl('', Validators.required),
+      objectives: new FormControl('', Validators.required),
+      results: new FormControl('', Validators.required),
+      highlights: new FormControl('', Validators.required),
+      action_area_description: new FormControl('', Validators.required),
+      action_area_id: new FormControl('', Validators.required),
+      initvStgId: new FormControl(this.initvStgId, Validators.required),
+    });
   }
 
-  onSave(narrativesForm): void {
-    console.log("GUARDANDO",narrativesForm.value);
+  ngOnInit(): void {
+    console.log(this.generalInformationForm);
+    this.activatedRoute.params.subscribe(resp => {
+      this.initvStgId = resp['id'];
+      this.initiativesSvc.initvStgId = resp['id'];
+      console.log('id del concept en narratives', resp['id']);
+      this.initiativesSvc.getConcept(this.initvStgId).subscribe(resp => {
+        console.log('response getConcept', resp)
+        this.generalInformationForm.controls['initvStgId'].setValue(this.initvStgId);
+        this.generalInformationForm.controls['name'].setValue(resp.data[0].conceptName);
+        this.generalInformationForm.controls['challenge'].setValue(resp.data[0].conceptChallenge);
+        this.generalInformationForm.controls['objectives'].setValue(resp.data[0].conceptObjectives);
+        this.generalInformationForm.controls['results'].setValue(resp.data[0].conceptResults);
+        this.generalInformationForm.controls['highlights'].setValue(resp.data[0].conceptHighlights);
+        this.generalInformationForm.controls['action_area_id'].setValue(resp.data[0].conceptActAreId);
+        this.generalInformationForm.controls['action_area_description'].setValue(resp.data[0].conceptActAreDes);
+        this.generalInformationForm.value.initvStgId = resp.data[0].initvStgId;
+        console.log(this.generalInformationForm);
+      });
+    });
+  }
+
+  updateConceptInfo(id) {
+    this.initiativesSvc.getActionAreaById(Number(this.generalInformationForm.value.action_area_id)).subscribe(resp => {
+      console.log('resp', resp);
+      this.initiativesSvc.updateConcept(this.generalInformationForm.value, resp, id).subscribe(resp => {
+        console.log('concept', resp);
+      })
+    })
+  }
+
+  getConceptInfo() {
+    this.initiativesSvc.getConcept(this.initvStgId).subscribe(resp => {
+      console.log('concept info', resp);
+      this.updateConceptInfo(resp.data[0].conceptInfoId);
+      console.log('si existe')
+    })
   }
 
 }
