90a8f7dc90fa5828831fa0ccdb8f035dd937ea3c
felipelvira <felipe.elvira.garcia@outlook.com>
2021-03-17 09:13:18 -0500

    Update: Create stages meta data updated

--- one-cgiar-back/src/migration/1614963554327-CreateStageMetadaTable.ts
+++ one-cgiar-back/src/migration/1614963554327-CreateStageMetadaTable.ts
@@ -4,10 +4,11 @@ export class CreateStageMetadaTable1614963554327 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         console.log('Add stages_meta');
+        let order_col = '`order` int(11) NOT NULL DEFAULT 0,'
         await queryRunner.query(`
             CREATE TABLE stages_meta (
                 id int(11) NOT NULL AUTO_INCREMENT,
-                order int(11) DEFAULT NULL,
+                ${order_col}               
                 group_by varchar(500) COLLATE utf8_bin DEFAULT NULL,
                 col_name varchar(250) COLLATE utf8_bin DEFAULT NULL,
                 display_name varchar(250) COLLATE utf8_bin DEFAULT NULL,
