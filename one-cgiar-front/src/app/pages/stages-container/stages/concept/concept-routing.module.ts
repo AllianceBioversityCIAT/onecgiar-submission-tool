9ccf320bdca1802cbe76405ba3c6192c26b900d4
Yecksin <yecksin@gmail.com>
2021-03-17 17:02:11 -0500

    the pages (general-information, narratives, initial-theory-of-change, work-packages, key-partners) are visible but not work correctly

--- one-cgiar-front/src/app/pages/stages-container/stages/concept/concept-routing.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/concept/concept-routing.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { GeneralInformationConceptComponent } from '@shared/components/concept/general-information-concept/general-information-concept.component';
+import { NarrativesConceptComponent } from '../../../../shared/components/concept/narratives-concept/narratives-concept.component';
+import { TheoryOfChangeComponent } from '../../../../shared/components/concept/theory-of-change/theory-of-change.component';
+import { WorkPackagesComponent } from '../../../../shared/components/concept/work-packages/work-packages.component';
+import { KeyPartnersComponent } from '../../../../shared/components/preconcept/key-partners/key-partners.component';
 
 const routes: Routes = [
   {
@@ -10,9 +14,26 @@ const routes: Routes = [
         path: 'general-information',
         component: GeneralInformationConceptComponent,
       },
+      {
+        path: 'narratives',
+        component: NarrativesConceptComponent,
+      },
+      {
+        path: 'initial-theory-of-change',
+        component: TheoryOfChangeComponent,
+      },
+      {
+        path: 'work-packages',
+        component: WorkPackagesComponent,
+      },
+      {
+        path: 'key-partners',
+        component: KeyPartnersComponent,
+      },
     ]
        
-  },
+  }
+  
 ];
 
 @NgModule({
