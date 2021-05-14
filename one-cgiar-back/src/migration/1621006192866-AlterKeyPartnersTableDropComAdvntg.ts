cb6fd795c671ec290310a82be2442ad821656a3f
felipelvira <felipe.elvira.garcia@outlook.com>
2021-05-14 14:11:09 -0500

    Update: Alter Key Partners drop comparative advantage

--- one-cgiar-back/src/migration/1621006192866-AlterKeyPartnersTableDropComAdvntg.ts
+++ one-cgiar-back/src/migration/1621006192866-AlterKeyPartnersTableDropComAdvntg.ts
@@ -4,7 +4,7 @@ export class AlterKeyPartnersTableDropComAdvntg1621006192866 implements Migratio
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         console.log('Alter key_partners')
-        // await queryRunner.query(`ALTER TABLE key_partners DROP COLUMN comparative_advantage`);
+        await queryRunner.query(`ALTER TABLE key_partners DROP COLUMN comparative_advantage`);
         await queryRunner.query(`ALTER TABLE key_partners DROP FOREIGN KEY FK_8b4c2233d99492cca2d96_initv_stages`);
         await queryRunner.query(`ALTER TABLE key_partners DROP COLUMN initvStgId`);
     }
