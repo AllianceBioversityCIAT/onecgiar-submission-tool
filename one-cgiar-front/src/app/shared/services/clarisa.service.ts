a6df641916efc334415a53b9c7283d20712c34be
Yecksin <yecksin@gmail.com>
2021-05-20 15:38:37 -0500

    test service was added in the home

--- one-cgiar-front/src/app/shared/services/clarisa.service.ts
+++ one-cgiar-front/src/app/shared/services/clarisa.service.ts
@@ -43,6 +43,10 @@ export class ClarisaService {
     return this.http.get<any>(`${environment.apiClarisa}/impact-areas-indicators`);
   }
 
+  test(){
+    return this.http.get<any>('https://jsonplaceholder.typicode.com/posts');
+  }
+
 
 
 }
