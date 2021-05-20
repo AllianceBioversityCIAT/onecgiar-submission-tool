14b3c373e3e43e693dfd21fb8d983eee79e8878f
Yecksin <yecksin@gmail.com>
2021-05-20 16:25:57 -0500

    Content-Security-Policy-Report-Only was added in back

--- one-cgiar-back/src/index.ts
+++ one-cgiar-back/src/index.ts
@@ -42,8 +42,8 @@ createConnection()
         app.use(express.json());
         app.use(function (req, res, next) {
         res.setHeader(
-            'Content-Security-Policy',
-            "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+            'Content-Security-Policy-Report-Only',
+            "default-src 'self' https://jsonplaceholder.typicode.com ; font-src 'self'; img-src 'self'; script-src 'self' https://jsonplaceholder.typicode.com ; style-src 'self'; frame-src 'self'"
         );
         next();
         });
