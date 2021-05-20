f61dca84777d49ad0c87424669fc983b24ddb306
Yecksin <yecksin@gmail.com>
2021-05-20 14:56:55 -0500

    Content-Security-Policy was added in back

--- one-cgiar-back/src/index.ts
+++ one-cgiar-back/src/index.ts
@@ -40,7 +40,13 @@ createConnection()
         const app = express();
         app.use(express.urlencoded({ extended: true }));
         app.use(express.json());
-
+        app.use(function (req, res, next) {
+        res.setHeader(
+            'Content-Security-Policy',
+            "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+        );
+        next();
+        });
         // middlewares
         startAccsCtrl();
         startMulter(parentDir);
