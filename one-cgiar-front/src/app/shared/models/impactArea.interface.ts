36347798f421361b5a01747e263231f395d0e374
Yecksin <yecksin@gmail.com>
2021-05-23 22:34:21 -0500

    data of projection of benefits saved were mapped

--- one-cgiar-front/src/app/shared/models/impactArea.interface.ts
+++ one-cgiar-front/src/app/shared/models/impactArea.interface.ts
@@ -1,8 +1,10 @@
 import { impactAreaIndicator } from './impactAreaIndicator.interface';
+import { ProjectionOfBenefits } from './projection-of-benefits.interface';
 export interface impactArea{
     description?: string
     id?: number
     indicators?: impactAreaIndicator []
     name?: string
+    projectedBenefits: ProjectionOfBenefits []
 }
 
