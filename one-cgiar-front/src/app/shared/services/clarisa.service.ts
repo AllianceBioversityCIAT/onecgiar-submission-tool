c323d08ac1e98a2d146e432173c83931b803aedd
Yecksin <yecksin@gmail.com>
2021-05-24 01:12:26 -0500

    Get regions and countries was rebuilt

--- one-cgiar-front/src/app/shared/services/clarisa.service.ts
+++ one-cgiar-front/src/app/shared/services/clarisa.service.ts
@@ -10,28 +10,28 @@ import { environment } from '../../../environments/environment';
 export class ClarisaService {
   constructor(public http: HttpClient) {}
 
-  // Query to get Institutions
-  getInstitutions(): Observable<any> {
-    return this.http.get<any>(`${environment.apiClarisa}/institutions`,{ responseType: 'json' });
-  }
+  // // Query to get Institutions
+  // getInstitutions(): Observable<any> {
+  //   return this.http.get<any>(`${environment.apiClarisa}/institutions`,{ responseType: 'json' });
+  // }
 
-  // Query to get regions
-  getRegions(): Observable<any> {
-    return this.http.get<any>(`${environment.apiClarisa}/un-regions`,{ responseType: 'json' }).pipe(map(resp=>{
-      // console.log('%cgetRegions','background: #222; color: #37ff73');
-      resp.map(region=>{region.code = region.um49Code;})
-      let res={response:{regions:resp}}
-      return res;
-    }));
-  }
+  // // Query to get regions
+  // getRegions(): Observable<any> {
+  //   return this.http.get<any>(`${environment.apiClarisa}/un-regions`,{ responseType: 'json' }).pipe(map(resp=>{
+  //     // console.log('%cgetRegions','background: #222; color: #37ff73');
+  //     resp.map(region=>{region.code = region.um49Code;})
+  //     let res={response:{regions:resp}}
+  //     return res;
+  //   }));
+  // }
 
-  // Query to get countries
-  getCountries(): Observable<any> {
-    return this.http.get<any>(`${environment.apiClarisa}/countries`,{ responseType: 'json' }).pipe(map(resp=>{
-      let res={response:{countries:resp}}
-      return res;
-    }));
-  }
+  // // Query to get countries
+  // getCountries(): Observable<any> {
+  //   return this.http.get<any>(`${environment.apiClarisa}/countries`,{ responseType: 'json' }).pipe(map(resp=>{
+  //     let res={response:{countries:resp}}
+  //     return res;
+  //   }));
+  // }
 
   // Query to get Impact areas
   getImpactAreas(): Observable<any> {
