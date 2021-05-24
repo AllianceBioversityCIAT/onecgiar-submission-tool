0ca030ac757d5154f3bca9a408a47617e6e89b47
Yecksin <yecksin@gmail.com>
2021-05-24 01:05:27 -0500

    projection-of-benefits.interface was updated

--- one-cgiar-front/src/app/shared/models/projection-of-benefits.interface.ts
+++ one-cgiar-front/src/app/shared/models/projection-of-benefits.interface.ts
@@ -1,13 +1,13 @@
 
 export interface ProjectionOfBenefits{
     active: number
-    created_at: string
+    created_at?: string
     id: number
     impact_area_id: number
     impact_area_indicator_id: number
     impact_area_indicator_name: string
     impact_area_name: string
     notes: string
-    updated_at: string
+    updated_at?: string
 }
 
