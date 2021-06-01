671bb642c3404abcfa68140ea1171d119c228c2e
felipelvira <felipe.elvira.garcia@outlook.com>
2021-06-01 15:42:08 -0500

    Update: JWT by Basic auth

--- one-cgiar-back/src/middlewares/jwt.ts
+++ one-cgiar-back/src/middlewares/jwt.ts
@@ -1,6 +1,5 @@
 import { Request, Response, NextFunction } from 'express'
 import * as jwt from 'jsonwebtoken'
-import config from '../config/config';
 import { utilLogin } from '../utils/auth-login';
 
 const jwtSecret = process.env.jwtSecret;
@@ -10,25 +9,24 @@ export const checkJwt = async (req: Request, res: Response, next: NextFunction) 
     // get credential if is application user
     const authBasic = req.headers.authorization;
     const credentials = authBasic ? authBasic.split('Basic ')[1] : null;
-    console.log(credentials, authBasic)
+    
     let jwtPayload, token_;
 
     try {
-
         if (credentials) {
             // decode nase 64 string
             let cre = Buffer.from(credentials, 'base64').toString('utf-8')
-            // // basic auth token creation
+            // basic auth token creation
             const email = cre.split(':')[0];
             const password = cre.split(':')[1];
             const { token } = await utilLogin(email, password);
+            // get token
             token_ = token;
         } else {
             // get SBT authorization token
             token_ = <string>req.headers['auth'];
         }
         
-        console.log(token_)
         jwtPayload = <any>jwt.verify(token_, jwtSecret);
         res.locals.jwtPayload = jwtPayload;
     } catch (error) {
