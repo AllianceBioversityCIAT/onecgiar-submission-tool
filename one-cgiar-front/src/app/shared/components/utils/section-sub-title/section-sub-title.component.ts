0790d7b61ae1c361dcc0278304f4655f71cd319b
Yecksin <yecksin@gmail.com>
2021-06-18 10:32:17 -0500

    section sub title component was created

---
+++ one-cgiar-front/src/app/shared/components/utils/section-sub-title/section-sub-title.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-section-sub-title',
+  templateUrl: './section-sub-title.component.html',
+  styleUrls: ['./section-sub-title.component.scss']
+})
+export class SectionSubTitleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
