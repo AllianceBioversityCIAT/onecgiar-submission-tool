67ab1cb4efed5c4a01fc89b74231483e8e5a383d
Yecksin <yecksin@gmail.com>
2021-06-23 15:35:50 -0500

    link-list component v2 was ended

--- one-cgiar-front/src/app/shared/components/custom-forms/link-list/link-list.component.ts
+++ one-cgiar-front/src/app/shared/components/custom-forms/link-list/link-list.component.ts
@@ -1,6 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { LinkListOptions } from '../../../models/forms-options/link-list-options.interface';
 
+interface LinkList {
+  link: string;
+  description: string;
+}
+
 @Component({
   selector: 'custom-link-list',
   templateUrl: './link-list.component.html',
@@ -8,9 +13,19 @@ import { LinkListOptions } from '../../../models/forms-options/link-list-options
 })
 export class LinkListComponent implements OnInit {
   @Input() options:LinkListOptions;
+  linkList: LinkList[] = []
   constructor() { }
 
   ngOnInit(): void {
+    this.joinSavedListAndLocalList();
+  }
+
+  joinSavedListAndLocalList(){
+    this.linkList = Object.assign(this.options.savedList)
+  }
+
+  addLink(){
+    this.linkList.push({link:'',description:''});
   }
 
 }
