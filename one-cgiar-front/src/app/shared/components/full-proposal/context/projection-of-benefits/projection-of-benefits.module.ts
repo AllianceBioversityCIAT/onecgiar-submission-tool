2abbe3a069e20a0b30f7cc9ebd215eb042416fe8
Yecksin <yecksin@gmail.com>
2021-07-14 08:32:57 -0500

    projection-of-benefits was moved to context

---
+++ one-cgiar-front/src/app/shared/components/full-proposal/context/projection-of-benefits/projection-of-benefits.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ProjectionOfBenefitsRoutingModule } from './projection-of-benefits-routing.module';
+import { ProjectionOfBenefitsComponent } from './projection-of-benefits.component';
+import { UtilsModule } from '../../../utils/utils.module';
+
+
+@NgModule({
+  declarations: [ProjectionOfBenefitsComponent],
+  imports: [
+    CommonModule,
+    ProjectionOfBenefitsRoutingModule,
+    UtilsModule
+  ]
+})
+export class ProjectionOfBenefitsModule { }
