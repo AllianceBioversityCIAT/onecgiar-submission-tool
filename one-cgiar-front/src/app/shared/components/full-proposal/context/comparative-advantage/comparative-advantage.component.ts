2b451b4a2c5f4dab58887f71ec48f80669bba2e1
Yecksin <yecksin@gmail.com>
2021-07-19 10:28:34 -0500

    save data of "comparative-advantage" was developed

--- one-cgiar-front/src/app/shared/components/full-proposal/context/comparative-advantage/comparative-advantage.component.ts
+++ one-cgiar-front/src/app/shared/components/full-proposal/context/comparative-advantage/comparative-advantage.component.ts
@@ -1,27 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { InitiativesService } from '../../../../services/initiatives.service';
-
+import { FullProposalService } from '../../../../services/full-proposal.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { InteractionsService } from '../../../../services/interactions.service';
 @Component({
   selector: 'app-comparative-advantage',
   templateUrl: './comparative-advantage.component.html',
   styleUrls: ['./comparative-advantage.component.scss']
 })
 export class ComparativeAdvantageComponent implements OnInit {
-  challengeStatementForm: FormGroup;
+  contextForm: FormGroup;
+  showfrom = false;
   constructor(
-    public _initiativesService:InitiativesService
+    public _initiativesService:InitiativesService,
+    public _fullProposalService:FullProposalService,
+    private spinnerService: NgxSpinnerService,
+    private _interactionsService:InteractionsService
   ) { 
-    this.challengeStatementForm = new FormGroup({
-      challengeStatement: new FormControl(''),
+    this.contextForm = new FormGroup({
+      comparative_advantage: new FormControl(null),
+      contextId:new FormControl(null),
     });
   }
 
   ngOnInit(): void {
+    this.getContext();
+    console.log(this.contextForm.value);
   }
 
   upserInfo(){
+    this._fullProposalService.patchContext(this._initiativesService.initiative.id,this.contextForm.value).subscribe(resp=>{
+      console.log(resp);
+      this.contextForm.valid?
+      this._interactionsService.successMessage('Comparative advantage has been saved'):
+      this._interactionsService.warningMessage('Comparative advantage has been saved, but there are incomplete fields')
+    })
+  }
 
+  getContext(){
+    this.spinnerService.show('spinner');
+    this._fullProposalService.getContext(this._initiativesService.initiative.id).subscribe(resp=>{
+      console.log(resp);
+      this.contextForm.controls['comparative_advantage'].setValue(resp?.response?.context?.comparative_advantage);
+      this.contextForm.controls['contextId'].setValue(resp?.response?.context?.id);
+      this.showfrom = true;
+      this.spinnerService.hide('spinner');
+    },err=>{
+      console.log("errorerekkasssssssssssssssdasda");
+    })
   }
 
 }
