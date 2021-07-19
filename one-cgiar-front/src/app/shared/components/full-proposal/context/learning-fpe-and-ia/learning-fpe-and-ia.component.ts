90abc0872cbb925340cc8f348f4077a0c8415cf3
Yecksin <yecksin@gmail.com>
2021-07-19 10:29:37 -0500

    save data of "learning-fpe-and-ia" was developed

--- one-cgiar-front/src/app/shared/components/full-proposal/context/learning-fpe-and-ia/learning-fpe-and-ia.component.ts
+++ one-cgiar-front/src/app/shared/components/full-proposal/context/learning-fpe-and-ia/learning-fpe-and-ia.component.ts
@@ -1,27 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { InitiativesService } from '../../../../services/initiatives.service';
-
+import { FullProposalService } from '../../../../services/full-proposal.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { InteractionsService } from '../../../../services/interactions.service';
 @Component({
   selector: 'app-learning-fpe-and-ia',
   templateUrl: './learning-fpe-and-ia.component.html',
   styleUrls: ['./learning-fpe-and-ia.component.scss']
 })
 export class LearningFpeAndIaComponent implements OnInit {
-  sectionForm: FormGroup;
+  contextForm: FormGroup;
+  showform = false;
   constructor(
-    public _initiativesService:InitiativesService
-  ) {
-    this.sectionForm = new FormGroup({
-      challengeStatement: new FormControl(''),
+    public _initiativesService:InitiativesService,
+    public _fullProposalService:FullProposalService,
+    private spinnerService: NgxSpinnerService,
+    private _interactionsService:InteractionsService
+  ) { 
+    this.contextForm = new FormGroup({
+      key_learnings: new FormControl(null),
+      contextId:new FormControl(null),
     });
-   }
+  }
 
   ngOnInit(): void {
+    this.getContext();
+    console.log(this.contextForm.value);
   }
 
-    upserInfo(){
-    
+  upserInfo(){
+    this._fullProposalService.patchContext(this._initiativesService.initiative.id,this.contextForm.value).subscribe(resp=>{
+      console.log(resp);
+      this.contextForm.valid?
+      this._interactionsService.successMessage('Comparative advantage has been saved'):
+      this._interactionsService.warningMessage('Comparative advantage has been saved, but there are incomplete fields')
+    })
   }
+
+  getContext(){
+    this.spinnerService.show('spinner');
+    this._fullProposalService.getContext(this._initiativesService.initiative.id).subscribe(resp=>{
+      console.log(resp);
+      this.contextForm.controls['key_learnings'].setValue(resp?.response?.context?.key_learnings);
+      this.contextForm.controls['contextId'].setValue(resp?.response?.context?.id);
+      this.showform = true;
+      this.spinnerService.hide('spinner');
+    },err=>{
+      console.log("errorerekkasssssssssssssssdasda");
+    })
+  }
+
 
 }
