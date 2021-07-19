d8ae6ebb1b5b06c7b3fd447f229b12636d3c2eb3
Yecksin <yecksin@gmail.com>
2021-07-19 10:30:34 -0500

    save data of "priority-setting" was developed

--- one-cgiar-front/src/app/shared/components/full-proposal/context/priority-setting/priority-setting.component.ts
+++ one-cgiar-front/src/app/shared/components/full-proposal/context/priority-setting/priority-setting.component.ts
@@ -1,26 +1,54 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { InitiativesService } from '../../../../services/initiatives.service';
-
+import { FullProposalService } from '../../../../services/full-proposal.service';
+import { NgxSpinnerService } from 'ngx-spinner';
+import { InteractionsService } from '../../../../services/interactions.service';
 @Component({
   selector: 'app-priority-setting',
   templateUrl: './priority-setting.component.html',
   styleUrls: ['./priority-setting.component.scss']
 })
 export class PrioritySettingComponent implements OnInit {
-  sectionForm: FormGroup;
+  contextForm: FormGroup;
+  showform = false;
   constructor(
-    public _initiativesService:InitiativesService
-  ) {
-    this.sectionForm = new FormGroup({
-      challengeStatement: new FormControl(''),
+    public _initiativesService:InitiativesService,
+    public _fullProposalService:FullProposalService,
+    private spinnerService: NgxSpinnerService,
+    private _interactionsService:InteractionsService
+  ) { 
+    this.contextForm = new FormGroup({
+      priority_setting: new FormControl(null),
+      contextId:new FormControl(null),
     });
-   }
+  }
 
   ngOnInit(): void {
+    this.getContext();
+    console.log(this.contextForm.value);
   }
+
   upserInfo(){
-    
+    this._fullProposalService.patchContext(this._initiativesService.initiative.id,this.contextForm.value).subscribe(resp=>{
+      console.log(resp);
+      this.contextForm.valid?
+      this._interactionsService.successMessage('Comparative advantage has been saved'):
+      this._interactionsService.warningMessage('Comparative advantage has been saved, but there are incomplete fields')
+    })
+  }
+
+  getContext(){
+    this.spinnerService.show('spinner');
+    this._fullProposalService.getContext(this._initiativesService.initiative.id).subscribe(resp=>{
+      console.log(resp);
+      this.contextForm.controls['priority_setting'].setValue(resp?.response?.context?.priority_setting);
+      this.contextForm.controls['contextId'].setValue(resp?.response?.context?.id);
+      this.showform = true;
+      this.spinnerService.hide('spinner');
+    },err=>{
+      console.log("errorerekkasssssssssssssssdasda");
+    })
   }
 
 }
