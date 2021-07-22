79459e2583b9ec6eaeee90393d90c02d6876bf88
Yecksin <yecksin@gmail.com>
2021-07-22 09:19:55 -0500

    General information FS component was created and added how container to full proposal stage and General information component was created to use in two stages dynamically

---
+++ one-cgiar-front/src/app/shared/components/general-information/general-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-general-information',
+  templateUrl: './general-information.component.html',
+  styleUrls: ['./general-information.component.scss']
+})
+export class GeneralInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
