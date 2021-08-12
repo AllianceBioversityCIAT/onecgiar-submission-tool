615655d6736ecc6caf5e91629e58bb9a5f3b637b
Yecksin <yecksin@gmail.com>
2021-08-12 11:21:34 -0500

    ibdc-simple-text component was added

--- one-cgiar-front/src/app/shared/components/custom-forms/custom-forms.module.ts
+++ one-cgiar-front/src/app/shared/components/custom-forms/custom-forms.module.ts
@@ -2,26 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from '@app/material.module';
 import { FormsModule,ReactiveFormsModule } from '@angular/forms';
-import { InputComponent } from './input/input.component';
-import { SelectComponent } from './select/select.component';
-import { NgxEditorModule } from 'ngx-editor';
 import { MultipleChoiceComponent } from './multiple-choice/multiple-choice.component';
-import { TextComponent } from './text/text.component';
 import { RegionsFilterPipe } from '../../pipes/regions-filter.pipe';
-import { UploadFilesComponent } from './upload-files/upload-files.component';
-import { LinkListComponent } from './link-list/link-list.component';
-
-
-
+import { SelectComponent } from './select/select.component';
 @NgModule({
-  declarations: [InputComponent, SelectComponent, MultipleChoiceComponent, TextComponent,RegionsFilterPipe, UploadFilesComponent, LinkListComponent],
+  declarations: [SelectComponent,MultipleChoiceComponent,RegionsFilterPipe],
   imports: [
     CommonModule,
     MaterialModule,
     FormsModule,
-    ReactiveFormsModule,
-    NgxEditorModule
+    ReactiveFormsModule
   ],
-  exports:      [ InputComponent, SelectComponent,MultipleChoiceComponent, TextComponent,UploadFilesComponent,LinkListComponent]
+  exports:      [SelectComponent, MultipleChoiceComponent]
 })
 export class CustomFormsModule { }
