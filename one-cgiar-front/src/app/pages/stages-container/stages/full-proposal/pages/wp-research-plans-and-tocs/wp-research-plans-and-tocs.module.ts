18d5bbc3c6d405a9611c1b86ffe65abe9264b88a
Yecksin <yecksin@gmail.com>
2021-08-31 16:27:43 -0500

    work package and work packages page was created in full proposal stge

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/wp-research-plans-and-tocs/wp-research-plans-and-tocs.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/wp-research-plans-and-tocs/wp-research-plans-and-tocs.module.ts
@@ -3,16 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { WpResearchPlansAndTocsRoutingModule } from './wp-research-plans-and-tocs-routing.module';
 import { WpResearchPlansAndTocsComponent } from '../wp-research-plans-and-tocs/wp-research-plans-and-tocs.component';
-import { UtilsModule } from '@app/shared/components/utils/utils.module';
 
 
 @NgModule({
   declarations: [WpResearchPlansAndTocsComponent],
   imports: [
     CommonModule,
-    WpResearchPlansAndTocsRoutingModule,
-    UtilsModule
-  ],
-  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    WpResearchPlansAndTocsRoutingModule
+  ]
 })
 export class WpResearchPlansAndTocsModule { }
