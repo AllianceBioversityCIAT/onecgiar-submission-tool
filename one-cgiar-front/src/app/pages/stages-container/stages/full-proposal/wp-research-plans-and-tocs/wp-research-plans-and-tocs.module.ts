8f6f21a8e2139ad5e7193f8ba2e9adecfeeedb43
Yecksin <yecksin@gmail.com>
2021-08-31 11:42:37 -0500

    Full proposal / wp section, title was added

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/wp-research-plans-and-tocs/wp-research-plans-and-tocs.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/wp-research-plans-and-tocs/wp-research-plans-and-tocs.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { WpResearchPlansAndTocsRoutingModule } from './wp-research-plans-and-tocs-routing.module';
 import { WpResearchPlansAndTocsComponent } from '../wp-research-plans-and-tocs/wp-research-plans-and-tocs.component';
+import { UtilsModule } from '../../../../../shared/components/utils/utils.module';
 
 
 @NgModule({
   declarations: [WpResearchPlansAndTocsComponent],
   imports: [
     CommonModule,
-    WpResearchPlansAndTocsRoutingModule
-  ]
+    WpResearchPlansAndTocsRoutingModule,
+    UtilsModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class WpResearchPlansAndTocsModule { }
