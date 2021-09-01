857122d68185d1ac7dd60e37337ba1221390560b
Yecksin <yecksin@gmail.com>
2021-09-01 11:39:59 -0500

    get work-package data was developed

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/wp-research-plans-and-tocs/fp-work-package/fp-work-package.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/wp-research-plans-and-tocs/fp-work-package/fp-work-package.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { InitiativesService } from '../../../../../../../shared/services/initiatives.service';
+import { DataControlService } from '../../../../../../../shared/services/data-control.service';
 
 @Component({
   selector: 'app-fp-work-package',
@@ -6,10 +10,71 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fp-work-package.component.scss']
 })
 export class FpWorkPackageComponent implements OnInit {
+  showForm = false;
+  workPackageForm: FormGroup;
+  geographicScope = {
+    regions: [],
+    countries: []
+  }
+  constructor(
+    public _initiativesService: InitiativesService,
+    private activatedRoute: ActivatedRoute,
+    private _dataControlService:DataControlService
+  ) {
+    this.workPackageForm = new FormGroup({
+      acronym: new FormControl(null),
+      name: new FormControl(null),
+      pathway_content: new FormControl(null),
+      is_global: new FormControl(true),
+    });
 
-  constructor() { }
+
+  }
 
   ngOnInit(): void {
+
+    this.activatedRoute.params.subscribe((resp: any) => {
+      this._initiativesService.getWpFpByInititative(resp.wpID).subscribe(resp => {
+        let directResp = resp.response.workpackage;
+        this.geographicScope.regions = directResp.regions;
+        this.geographicScope.countries = directResp.countries;
+        this.updateFields(directResp);
+        this._initiativesService.getCLARISARegions('').subscribe(regions=>{
+          this.geographicScope.regions.map(mapReg=>{
+            regions.response.regions.forEach(regionItem=>{
+              if (regionItem.um49Code == mapReg.region_id) mapReg.name = regionItem.name;
+            })
+          })
+          this._dataControlService.showRegions = true;
+        })
+  
+        this._initiativesService.getCLARISACountries().subscribe(countries=>{       
+          this.geographicScope.countries.map(mapCoun=>{
+            countries.response.countries.forEach(countryItem=>{
+              if (countryItem.code == mapCoun.country_id) mapCoun.name = countryItem.name;
+            })
+            
+          })
+          this._dataControlService.showCountries = true;
+        })
+        console.log(directResp);
+      })
+
+      console.log(resp.wpID);
+    });
+
+
+  }
+
+  updateFields(directResp){
+        this.workPackageForm.controls['acronym'].setValue(directResp.acronym);
+        this.workPackageForm.controls['name'].setValue(directResp.name);
+        this.workPackageForm.controls['pathway_content'].setValue(directResp.pathway_content);
+        this.showForm = false;
+        setTimeout(() => {
+          this.showForm = true;
+        }, 100);
+        
   }
 
 }
--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/wp-research-plans-and-tocs/fp-work-package/fp-work-package.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/wp-research-plans-and-tocs/fp-work-package/fp-work-package.module.ts
@@ -4,14 +4,18 @@ import { CommonModule } from '@angular/common';
 import { FpWorkPackageRoutingModule } from './fp-work-package-routing.module';
 import { FpWorkPackageComponent } from './fp-work-package.component';
 import { UtilsModule } from '../../../../../../../shared/components/utils/utils.module';
-
+import { GeographicScopeModule } from '../../../../shared/components/geographic-scope/geographic-scope.module';
+// import { IbdAngularComponentsModule } from '../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
 
 @NgModule({
   declarations: [FpWorkPackageComponent],
   imports: [
     CommonModule,
     FpWorkPackageRoutingModule,
-    UtilsModule
+    UtilsModule,
+    GeographicScopeModule,
+    IbdAngularComponentsModule
   ],
   schemas:[CUSTOM_ELEMENTS_SCHEMA]
 })
