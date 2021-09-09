c8405e9d53aad30a452c2c53bd5f351ea92f06ab
Yecksin Mauricio <yecksin@gmail.com>
2021-09-09 12:14:48 -0500

    ibdc-upload-files was added in melia sub sections

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/melia-studies-and-activities/melia-studies-and-activities.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/melia-studies-and-activities/melia-studies-and-activities.module.ts
@@ -4,14 +4,16 @@ import { CommonModule } from '@angular/common';
 import { MeliaStudiesAndActivitiesRoutingModule } from './melia-studies-and-activities-routing.module';
 import { MeliaStudiesAndActivitiesComponent } from './melia-studies-and-activities.component';
 import { UtilsModule } from '../../../../../../../shared/components/utils/utils.module';
-
+// import { IbdAngularComponentsModule } from '../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
 
 @NgModule({
   declarations: [MeliaStudiesAndActivitiesComponent],
   imports: [
     CommonModule,
     MeliaStudiesAndActivitiesRoutingModule,
-    UtilsModule
+    UtilsModule,
+    IbdAngularComponentsModule
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
