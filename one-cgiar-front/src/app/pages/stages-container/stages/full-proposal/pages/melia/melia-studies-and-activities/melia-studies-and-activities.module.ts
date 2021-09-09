b5a96cf99229f4a4122c4472a546ff8d291f59e1
Yecksin Mauricio <yecksin@gmail.com>
2021-09-09 09:54:15 -0500

    Section title was added in melia sub sections

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/melia-studies-and-activities/melia-studies-and-activities.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/melia-studies-and-activities/melia-studies-and-activities.module.ts
@@ -1,15 +1,18 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MeliaStudiesAndActivitiesRoutingModule } from './melia-studies-and-activities-routing.module';
 import { MeliaStudiesAndActivitiesComponent } from './melia-studies-and-activities.component';
+import { UtilsModule } from '../../../../../../../shared/components/utils/utils.module';
 
 
 @NgModule({
   declarations: [MeliaStudiesAndActivitiesComponent],
   imports: [
     CommonModule,
-    MeliaStudiesAndActivitiesRoutingModule
-  ]
+    MeliaStudiesAndActivitiesRoutingModule,
+    UtilsModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class MeliaStudiesAndActivitiesModule { }
--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/result-framework/result-framework.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/result-framework/result-framework.module.ts
@@ -1,15 +1,19 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ResultFrameworkRoutingModule } from './result-framework-routing.module';
 import { ResultFrameworkComponent } from './result-framework.component';
+import { UtilsModule } from '@app/shared/components/utils/utils.module';
+
 
 
 @NgModule({
   declarations: [ResultFrameworkComponent],
   imports: [
     CommonModule,
-    ResultFrameworkRoutingModule
-  ]
+    ResultFrameworkRoutingModule,
+    UtilsModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class ResultFrameworkModule { }
