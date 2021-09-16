383ff56899c0811d01beb38b3473dfbfc21aad87
Yecksin Mauricio <yecksin@gmail.com>
2021-09-15 23:44:13 -0500

    \result-framework.module import was corrected

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/result-framework/result-framework.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/result-framework/result-framework.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ResultFrameworkRoutingModule } from './result-framework-routing.module';
 import { ResultFrameworkComponent } from './result-framework.component';
 import { UtilsModule } from '@app/shared/components/utils/utils.module';
-import { IbdAngularComponentsModule } from '../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
-// import { IbdAngularComponentsModule } from 'ibd-angular-components';
+// import { IbdAngularComponentsModule } from '../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
