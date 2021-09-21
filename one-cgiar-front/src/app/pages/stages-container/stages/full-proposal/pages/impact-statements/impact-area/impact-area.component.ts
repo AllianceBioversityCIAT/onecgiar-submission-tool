d7d6104e8e9b5247d911e075b0a63d4235b5b466
Yecksin Mauricio <yecksin@gmail.com>
2021-09-21 11:39:03 -0500

    impact-area subsection was created in the impact-statements section

---
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/impact-statements/impact-area/impact-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-impact-area',
+  templateUrl: './impact-area.component.html',
+  styleUrls: ['./impact-area.component.scss']
+})
+export class ImpactAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
