730152576eebfb7b01c9dc8cd6f4678de63a218d
Yecksin Mauricio <yecksin@gmail.com>
2021-10-04 12:22:27 -0500

    hide or show dev-tag depending of the production enviroment value

--- one-cgiar-front/src/app/app.component.ts
+++ one-cgiar-front/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { UtilsService } from './shared/services/utils.service';
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -12,10 +13,11 @@ import { Subject } from 'rxjs';
 export class AppComponent implements OnInit, OnDestroy {
   opened = false;
   private destroy$ = new Subject<any>();
-
+  isDevModeToShowTag = false;
   constructor(private utilsSvc: UtilsService, public router: Router) {}
 
   ngOnInit(): void {
+    this.isDevModeToShowTag = environment.production;
     this.utilsSvc.sidebarOpened$
       .pipe(takeUntil(this.destroy$))
       .subscribe((res: boolean) => (this.opened = res));
