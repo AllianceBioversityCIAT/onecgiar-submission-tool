8d11877ee10cd0dc3071fa840f3d82cc08cd1354
JuanCode <46978099+JuankCadavid@users.noreply.github.com>
2021-10-08 14:01:26 -0500

    Create 1633719545592-AlterTableInitiatevesAddColumnAcronym.ts

---
+++ one-cgiar-back/src/migration/1633719545592-AlterTableInitiatevesAddColumnAcronym.ts
@@ -0,0 +1,17 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AlterTableInitiatevesAddColumnAcronym1633719545592 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.query(`
+        ALTER TABLE initiatives
+        ADD COLUMN acronym TEXT NOT NULL
+        `);
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
