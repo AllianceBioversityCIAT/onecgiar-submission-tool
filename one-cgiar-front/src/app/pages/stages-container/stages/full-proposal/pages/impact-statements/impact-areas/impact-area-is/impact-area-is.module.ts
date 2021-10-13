e2dee6ace2002ebdde7a047606b67fd10f828699
Yecksin Mauricio <yecksin@gmail.com>
2021-10-13 15:20:36 -0500

    Import corrected

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/impact-statements/impact-areas/impact-area-is/impact-area-is.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/impact-statements/impact-areas/impact-area-is/impact-area-is.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ImpactAreaIsRoutingModule } from './impact-area-is-routing.module';
 import { ImpactAreaIsComponent } from './impact-area-is.component';
 import { UtilsModule } from '../../../../../../../../shared/components/utils/utils.module';
-import { IbdAngularComponentsModule } from '../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
-// import { IbdAngularComponentsModule } from 'ibd-angular-components';
+// import { IbdAngularComponentsModule } from '../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GlobalPartnersRequestModule } from '../../../../../shared/components/global-partners-request/global-partners-request.module';
 
