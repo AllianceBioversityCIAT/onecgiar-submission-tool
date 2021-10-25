9c4b8781b16b4acfe520dea30dbe6f04eca84cdf
Yecksin Mauricio <yecksin@gmail.com>
2021-10-25 08:55:52 -0500

    custom-steper component added and developed

--- one-cgiar-front/src/app/pages/stages-container/stages/shared/components/custom-steper/custom-steper.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/shared/components/custom-steper/custom-steper.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 interface RouteData{
   route:string;
   title:string;
@@ -17,9 +17,10 @@ interface Steper{
 export class CustomSteperComponent implements OnInit {
   @Input() options:Steper;
 
-  constructor() { }
+  constructor(
+    public router:Router
+  ) { }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
 }
