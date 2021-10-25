27805849697a17cb431314501ff347720632c021
JuanCode <46978099+JuankCadavid@users.noreply.github.com>
2021-10-25 15:46:09 -0500

    Update tawk-to.component.ts

--- one-cgiar-front/src/app/shared/components/tawk-to/tawk-to.component.ts
+++ one-cgiar-front/src/app/shared/components/tawk-to/tawk-to.component.ts
@@ -32,7 +32,7 @@ export class TawkToComponent implements OnInit {
       (function(){
       var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
       s1.async=true;
-      s1.src = 'https://embed.tawk.to/${this.config.tawkToId}/1fgm8aj51';
+      s1.src = 'https://embed.tawk.to/${this.config.tawkToId}';
       s1.charset='UTF-8';
       s1.setAttribute('crossorigin','*');
       s0.parentNode.insertBefore(s1,s0);
