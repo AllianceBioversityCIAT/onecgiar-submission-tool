c27420337c5705978c2799f95ddb21a5e7df9fd8
Yecksin Mauricio <yecksin@gmail.com>
2021-10-26 14:50:54 -0500

    get data in table B

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/result-framework/re-fre-impact-area/rf-table-b/rf-table-b.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/result-framework/re-fre-impact-area/rf-table-b/rf-table-b.component.ts
@@ -8,26 +8,35 @@ import { ReFreDataControlService } from '../services/re-fre-data-control.service
   styleUrls: ['./rf-table-b.component.scss']
 })
 export class RfTableBComponent implements OnInit {
-  indicatorsList:[];
-  indicatorsListLoaded=false;
+  outcomesIndicatorsList:[];
+  outcomesIndicatorsIsLoaded=false;
   constructor(
     public _initiativesService:InitiativesService,
     private _reFreDataControlService:ReFreDataControlService
   ) { }
 
   ngOnInit(): void {
-    this.getProjectedBenefitLists(this._reFreDataControlService.impactAreaID);
+    // this.getProjectedBenefitLists(this._reFreDataControlService.impactAreaID);
+    this.getOutcomesIndicators();
   }
 
-  getProjectedBenefitLists(impactAreaId){
-    this._initiativesService.getProjectedBenefitLists().subscribe(resp=>{
-      // console.log("getProjectedBenefitLists");
-      // console.log(resp);
-      
-      this.indicatorsList = resp.response.impactProjectedBenefitsRequested.filter(item=>item.impactAreaId == impactAreaId && item.isApplicableProjectedBenefits == true);
-      console.log(this.indicatorsList);
-    },err=>{},()=>this.indicatorsListLoaded =  true)
-    
+  getOutcomesIndicators(){
+    this._initiativesService.getOutcomesIndicators().subscribe(resp=>{
+      this.outcomesIndicatorsList = resp.response.outcomesIndicators;
+      console.log(resp.response.outcomesIndicators);
+      this.outcomesIndicatorsIsLoaded =  true;
+    })
   }
+
+  // getProjectedBenefitLists(impactAreaId){
+  //   this._initiativesService.getProjectedBenefitLists().subscribe(resp=>{
+  //     // console.log("getProjectedBenefitLists");
+  //     // console.log(resp);
+      
+  //     this.indicatorsList = resp.response.impactProjectedBenefitsRequested.filter(item=>item.impactAreaId == impactAreaId && item.isApplicableProjectedBenefits == true);
+  //     console.log(this.indicatorsList);
+  //   },err=>{},()=>this.indicatorsListLoaded =  true)
+    
+  // }
 
 }
