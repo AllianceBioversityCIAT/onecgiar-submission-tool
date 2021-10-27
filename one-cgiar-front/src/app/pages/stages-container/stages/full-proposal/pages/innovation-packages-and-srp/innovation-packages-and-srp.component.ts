84085be86d55c06eed465d12c4dee2c4c3059424
Yecksin Mauricio <yecksin@gmail.com>
2021-10-27 11:45:09 -0500

    Innovation Packages and Scaling Readiness Plan validations added

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/innovation-packages-and-srp/innovation-packages-and-srp.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/innovation-packages-and-srp/innovation-packages-and-srp.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { InteractionsService } from '@app/shared/services/interactions.service';
 import { InitiativesService } from '../../../../../../shared/services/initiatives.service';
 import { DataControlService } from '../../../../../../shared/services/data-control.service';
+import { DataValidatorsService } from '../../../shared/data-validators.service';
 
 @Component({
   selector: 'app-innovation-packages-and-srp',
@@ -12,10 +13,12 @@ import { DataControlService } from '../../../../../../shared/services/data-contr
 export class InnovationPackagesAndSrpComponent implements OnInit {
   secionForm: FormGroup;
   showForm = false;
+  extraValidation = false;
   constructor(
     public _initiativesService:InitiativesService,
     private _interactionsService:InteractionsService,
-    private _dataControlService:DataControlService
+    private _dataControlService:DataControlService,
+    private _dataValidatorsService:DataValidatorsService
   ) {
     this.secionForm = new FormGroup({
       id: new FormControl(null),
@@ -25,6 +28,7 @@ export class InnovationPackagesAndSrpComponent implements OnInit {
 
   ngOnInit(): void {
     this.getInnovationPackages()
+    this.formChanges();
   }
   getInnovationPackages(){
     this._initiativesService.getInnovationPackages(this._initiativesService.initiative.id).subscribe(resp=>{
@@ -45,6 +49,13 @@ export class InnovationPackagesAndSrpComponent implements OnInit {
       this.secionForm.valid?
       this._interactionsService.successMessage('Innovation Packages and Scaling Readiness Plan has been saved'):
       this._interactionsService.warningMessage('Innovation Packages and Scaling Readiness Plan has been saved, but there are incomplete fields')
+    })
+  }
+
+  formChanges(){
+    this.secionForm.valueChanges.subscribe(resp=>{
+      console.log("changes");
+      this.extraValidation = this._dataValidatorsService.wordCounterIsCorrect(this.secionForm.get("key_principles").value, 250);
     })
   }
 
