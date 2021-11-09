c9cf1ae8a1cda8d8e508faf3e6c288e45a600eb4
JuanCode <46978099+JuankCadavid@users.noreply.github.com>
2021-11-09 08:24:03 -0500

    JC - add preview partners to Docs

--- one-cgiar-back/src/routes/FullProposalRoutes.ts
+++ one-cgiar-back/src/routes/FullProposalRoutes.ts
@@ -1216,6 +1216,57 @@ router.get("/innovation-packages/:initiativeId([0-9]+)", [checkJwt, checkRole('i
 /**
  * GET PREVIEW PARTNERS PER INITIATIVE
  */
+/**
+ * @api {get} stages-control/proposal/preview-partners/:initiativeId Previews - Request Partners per Initiative
+ * @apiVersion 1.0.2
+ * @apiPermission admin
+ * @apiName GetPreviewPartners
+ * @apiGroup Proposal
+ * 
+ * @apiDescription  Shows Preview Partners per Initiative
+ * 
+ * @apiExample Example usage:
+ * https://initiativestest.ciat.cgiar.org/api/stages-control/proposal/preview-partners/1
+ * 
+ * @apiSampleRequest https://initiativestest.ciat.cgiar.org/api/stages-control/proposal/preview-partners/1
+ *
+ * @apiHeader {String} auth
+ * 
+ * @apiSuccessExample Success-Response:
+ *     HTTP/1.1 200 OK
+ * {
+ *     "response": {
+ *         "previewPartners": [
+ *             {
+ *                 "partner_name": "Wageningen University and Research Centre",
+ *                 "url": "http://www.wur.nl/en.htm",
+ *                 "acronym": "WUR",
+ *                 "initiative_id": "INIT-1",
+ *                 "action_area": "Genetic Innovation",
+ *                 "partner_id": "",
+ *                 "location": "",
+ *                 "organization_type_IATI": "",
+ *                 "network_mapping_codes": "",
+ *                 "organization_type_clarisa": "University",
+ *                 "clarisa_id": 36,
+ *                 "demand": 0,
+ *                 "innovation": 0,
+ *                 "scaling": 0,
+ *                 "hq_location_clarisa": "NL",
+ *                 "impact_area_id": 2,
+ *                 "Source": "impact_satatements"
+ *             }
+ *         ]
+ *     },
+ *     "title": "Full Proposal:Preview Partners"
+ * }
+ *
+ * @apiError Error : Get Preview Partners: Full proposal
+ *
+ * @apiErrorExample Error-Response:
+ *     HTTP/1.1 400 Not Found
+ *     { message: "Get Preview Partners: Full proposal", error }
+ */
  router.get("/preview-partners/:initiativeId([0-9]+)", [checkJwt, checkRole('strategies', 'readOwn')], stagefull.getPreviewPartners);
 
 
