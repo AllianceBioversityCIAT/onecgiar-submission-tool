5c345d0a190e05258f7f2f93a30dc19097d5aa9d
Yecksin Mauricio <yecksin@gmail.com>
2021-11-23 08:48:20 -0500

    Risk Score was improved

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/mpara/risk-assessment/components/risk-assessment-item/risk-assessment-item.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/mpara/risk-assessment/components/risk-assessment-item/risk-assessment-item.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RiskAssessmentItemComponent } from './risk-assessment-item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { IbdAngularComponentsModule } from 'ibd-angular-components';
-import {ButtonModule} from 'primeng/button';
+// import { IbdAngularComponentsModule } from 'ibd-angular-components';
+import { IbdAngularComponentsModule } from '../../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+
+import { ButtonModule } from 'primeng/button';
 import { OpportunityItemModule } from '../opportunity-item/opportunity-item.module';
 import { ButtonEditOrDeleteModule } from '../../../../../../shared/components/button-edit-or-delete/button-edit-or-delete.module';
 
