4c25551cb23be67106c4072113b9e5e3cbe61e89
JuanCode <46978099+JuankCadavid@users.noreply.github.com>
2021-12-06 14:36:18 -0500

    JC - add migration for alter table risk assessment

---
+++ one-cgiar-back/src/migration/1638819165879-AlterTableRiskAssessmentTheme.ts
@@ -0,0 +1,18 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AlterTableRiskAssessmentTheme1638819165879 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        console.log('alter Risk Assessment table fiel risks_theme');
+
+        await queryRunner.query(`
+                ALTER TABLE risk_assessment MODIFY risks_theme text DEFAULT NULL
+            `);
+
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
