ccc85b46a4cb8c060bdaed4769d704c180598534
Yecksin Mauricio <yecksin@gmail.com>
2021-12-06 15:44:47 -0500

    imports corrected

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/mpara/risk-assessment/components/opportunity-item/opportunity-item.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/mpara/risk-assessment/components/opportunity-item/opportunity-item.module.ts
@@ -1,8 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { OpportunityItemComponent } from './opportunity-item.component';
-// import { IbdAngularComponentsModule } from 'ibd-angular-components';
-import { IbdAngularComponentsModule } from './../../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
+// import { IbdAngularComponentsModule } from './../../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 
--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/mpara/risk-assessment/components/risk-assessment-item/risk-assessment-item.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/mpara/risk-assessment/components/risk-assessment-item/risk-assessment-item.module.ts
@@ -2,8 +2,8 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RiskAssessmentItemComponent } from './risk-assessment-item.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-// import { IbdAngularComponentsModule } from 'ibd-angular-components';
-import { IbdAngularComponentsModule } from '../../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
+// import { IbdAngularComponentsModule } from '../../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
 
 import { ButtonModule } from 'primeng/button';
 import { OpportunityItemModule } from '../opportunity-item/opportunity-item.module';
