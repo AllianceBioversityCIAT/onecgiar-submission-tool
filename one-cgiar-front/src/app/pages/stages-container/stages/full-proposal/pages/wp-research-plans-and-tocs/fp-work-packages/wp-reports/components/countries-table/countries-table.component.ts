61bdf2e39fd44440f0d89b79c0599659be57626d
Yecksin Mauricio <yecksin@gmail.com>
2021-12-16 09:37:07 -0500

    Geographic scope preview - Improved

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/wp-research-plans-and-tocs/fp-work-packages/wp-reports/components/countries-table/countries-table.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/wp-research-plans-and-tocs/fp-work-packages/wp-reports/components/countries-table/countries-table.component.ts
@@ -11,8 +11,8 @@ import { ManageExcelService } from '../../../../../../services/manage-excel.serv
 export class CountriesTableComponent implements OnInit {
 
   @Input() countries:Country[]=[];
-  headerPreviewPartners = ['um49code', 'isoAlpha2', 'name'];
-
+  headerPreviewPartners = ['code', 'isoAlpha2', 'name'];
+  headerPreviewTitles= ['code', 'ISO Alpha2','acronym'];
 
   constructor(
     private _manageExcelService:ManageExcelService
@@ -20,21 +20,22 @@ export class CountriesTableComponent implements OnInit {
 
   ngOnInit(): void {
     this.countries.map((item:any)=>delete item.official_code);
-    this.exampleasa();
+    console.log(this.countries)
   }
 
   exportBasicExcel(){
     this._manageExcelService.exportBasicExcel(this.countries,'Countries preview',[{wpx:90},{wpx:200},{wpx:90}])
   }
 
-  exampleasa(){
-    // import { saveAs } from 'file-saver';
+  //! Other library to add styles in xlx
+  // exampleasa(){
+  //   // import { saveAs } from 'file-saver';
 
-    import("file-saver").then(saveAs => {
-      var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
-      saveAs(blob, "hello world.txt");
-    });
+  //   import("file-saver").then(saveAs => {
+  //     var blob = new Blob(["Hello, world!"], {type: "text/plain;charset=utf-8"});
+  //     saveAs(blob, "hello world.txt");
+  //   });
 
-  }
+  // }
 
 }
