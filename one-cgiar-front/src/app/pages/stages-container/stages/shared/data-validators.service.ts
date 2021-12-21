3e808a3b49e868cc06bf391a9aa73ba2260e4b5b
Yecksin Mauricio <yecksin@gmail.com>
2021-12-21 16:04:10 -0500

    General information validations added

--- one-cgiar-front/src/app/pages/stages-container/stages/shared/data-validators.service.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/shared/data-validators.service.ts
@@ -113,31 +113,66 @@ export class DataValidatorsService {
 
   }
 
+  //! DEPRECARED
+  // wordCounterIsCorrect(textTocount,maxWords) {
+  //   let words = 0;
+  //   if (textTocount) {
+  //     let textReplaced = textTocount.replace(/(<(\/?p)>)/gi,' ').replace(/(<([^>]+)>)/gi,'');
+  //     // console.log(textReplaced);
+  //     if (textReplaced) {
+  //       let textMatch = textReplaced.match(/\S+/g);
+  //       if (textMatch) {
+  //         words = textMatch.length;
+  //       }else{
+  //         return true;
+  //       }
+        
+  //       if (words > maxWords) {
+  //         return false;
+  //       }else{
+  //         return true;
+  //       }
+  //     }else{
+  //       return true;
+  //     }
+
+  //   }else{
+  //     return true;
+  //   }
+
+  // }
+
+
+
+  
+
+
+
   wordCounterIsCorrect(textTocount,maxWords) {
     let words = 0;
     if (textTocount) {
-      let textReplaced = textTocount.replace(/(<(\/?p)>)/gi,' ').replace(/(<([^>]+)>)/gi,'');
-      // console.log(textReplaced);
-      if (textReplaced) {
-        let textMatch = textReplaced.match(/\S+/g);
-        if (textMatch) {
-          words = textMatch.length;
-        }else{
-          return true;
-        }
-        
+      let textReplaced = textTocount.replace(/(<(\/?p)>)|(&nbsp;)/gi, ' ').replace(/(<([^>]+)>)/gi, '');
+      let splitWords = textReplaced.split(' ')
+      if (splitWords.length) {
+        words = 0;
+        splitWords.map(item => {
+          if (item === '' || item === '\n' || item === '\t') return;
+          words++
+        })
+
         if (words > maxWords) {
           return false;
         }else{
           return true;
         }
-      }else{
+      } else {
         return true;
       }
 
-    }else{
+    } else {
       return true;
     }
+
 
   }
 
