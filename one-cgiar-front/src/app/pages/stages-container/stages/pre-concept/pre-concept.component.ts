b1c7adc66fa9cc3b28293689dd0aa0da750f0b23
Yecksin Mauricio <yecksin@gmail.com>
2022-01-07 09:26:06 -0500

    pre concept component added

---
+++ one-cgiar-front/src/app/pages/stages-container/stages/pre-concept/pre-concept.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pre-concept',
+  templateUrl: './pre-concept.component.html',
+  styleUrls: ['./pre-concept.component.scss']
+})
+export class PreConceptComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
