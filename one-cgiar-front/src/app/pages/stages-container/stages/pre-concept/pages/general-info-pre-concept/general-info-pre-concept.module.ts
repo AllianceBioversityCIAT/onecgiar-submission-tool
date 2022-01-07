b1a7685ccb98b85d8f667dab46ae5884593d9f07
Yecksin Mauricio <yecksin@gmail.com>
2022-01-07 11:17:03 -0500

    General info component added in general info pre concept

--- one-cgiar-front/src/app/pages/stages-container/stages/pre-concept/pages/general-info-pre-concept/general-info-pre-concept.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/pre-concept/pages/general-info-pre-concept/general-info-pre-concept.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { GeneralInfoPreConceptRoutingModule } from './general-info-pre-concept-routing.module';
 import { GeneralInfoPreConceptComponent } from './general-info-pre-concept.component';
+import { GeneralInformationModule } from '../../../shared/components/general-information/general-information.module';
 
 
 @NgModule({
   declarations: [GeneralInfoPreConceptComponent],
   imports: [
     CommonModule,
-    GeneralInfoPreConceptRoutingModule
+    GeneralInfoPreConceptRoutingModule,
+    GeneralInformationModule
   ]
 })
 export class GeneralInfoPreConceptModule { }
