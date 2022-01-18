534877fc444045ef0e532ee5a1ed806cc735a1d4
Yecksin Mauricio <yecksin@gmail.com>
2022-01-18 15:21:40 -0500

    Add button component added

--- one-cgiar-front/src/app/pages/stages-container/stages/pre-concept/pages/pc-common-modules.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/pre-concept/pages/pc-common-modules.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 import { UtilsModule } from '../../../../../shared/components/utils/utils.module';
 import { IbdAngularComponentsModule } from 'ibd-angular-components';
 import { InitiativesService } from '../../../../../shared/services/initiatives.service';
+import { AddButtonModule } from '../../../../../shared/components/add-button/add-button.module';
+import { ButtonAddElementModule } from '../../shared/components/button-add-element/button-add-element.module';
 
 
-
+let modules = [
+  IbdAngularComponentsModule,
+  AddButtonModule,
+  ButtonAddElementModule
+]
 @NgModule({
   declarations: [],
-  exports:[UtilsModule,IbdAngularComponentsModule],
-  imports: [
-    CommonModule,
-    IbdAngularComponentsModule
-  ],
+  exports:[UtilsModule,...modules],
+  imports: [CommonModule,...modules],
   providers:[ InitiativesService ]
 })
 export class PcCommonModulesModule { }
