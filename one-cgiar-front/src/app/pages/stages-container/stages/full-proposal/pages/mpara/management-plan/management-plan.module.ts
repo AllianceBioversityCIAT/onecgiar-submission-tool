236db53ada4729ee97d598a29bed005cfa44f4bb
Yecksin Mauricio <yecksin@gmail.com>
2022-01-27 07:55:25 -0500

    IbdAngularComponentsModule correct import

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/mpara/management-plan/management-plan.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/mpara/management-plan/management-plan.module.ts
@@ -4,8 +4,8 @@ import { CommonModule } from '@angular/common';
 import { ManagementPlanRoutingModule } from './management-plan-routing.module';
 import { ManagementPlanComponent } from './management-plan.component';
 import { UtilsModule } from '@app/shared/components/utils/utils.module';
-import { IbdAngularComponentsModule } from '../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
-// import { IbdAngularComponentsModule } from 'ibd-angular-components';
+// import { IbdAngularComponentsModule } from '../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
