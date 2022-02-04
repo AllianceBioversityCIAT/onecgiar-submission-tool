9e73330e99eb3170e16495e72da885eacb3ab494
Yecksin Mauricio <yecksin@gmail.com>
2022-02-04 09:20:12 -0500

    createInitiative v1

--- one-cgiar-front/src/app/pages/stages-container/stages/shared/components/initiative-creator/initiative-creator.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/shared/components/initiative-creator/initiative-creator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { InitiativesService } from '../../../../../../shared/services/initiatives.service';
+import { DataControlService } from '../../../../../../shared/services/data-control.service';
 
 @Component({
   selector: 'app-initiative-creator',
@@ -9,7 +11,11 @@ export class InitiativeCreatorComponent implements OnInit {
   renderDilog: boolean = false;
   display: boolean = false;
 
-  constructor() { }
+
+  constructor(
+    private _initiativesService:InitiativesService,
+    private _dataControlService:DataControlService
+  ) { }
 
   ngOnInit(): void {
   }
@@ -17,6 +23,25 @@ export class InitiativeCreatorComponent implements OnInit {
   showDialog() {
       this.renderDilog = true;
       this.display = true;
+  }
+
+  createInitiative(){
+    
+    let body = {
+      name:'Hola mundo',
+      action_area_description:'Action area',
+      action_area_id: 1,
+      active: true,
+      acronym: 'acronym',
+      generalInformationId: null
+    }
+
+    console.log(body);
+
+    this._initiativesService.patchGeneralInformation(41, this._dataControlService.getStageRouteByStageId(2).ownPath, body).subscribe(resp=>{
+      console.log(resp);
+    })
+
   }
 
 }
