5f568b1319ae85c14b004f7cd9218bdf840dc41b
Yecksin Mauricio <yecksin@gmail.com>
2022-02-09 10:25:56 -0500

    dev-tag module added and not-found-routing improved

--- one-cgiar-front/src/app/app.module.ts
+++ one-cgiar-front/src/app/app.module.ts
@@ -5,7 +5,6 @@ import { CUSTOM_ELEMENTS_SCHEMA, ErrorHandler, NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from '@shared/components/header/header.component';
 import { MaterialModule } from '@app/material.module';
 import { SidebarModule } from '@shared/components/sidebar/sidebar.module';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
@@ -17,10 +16,8 @@ import { ErrorInterceptor } from '@shared/interceptors/error.interceptor';
 import { AppErrorHandler } from '@shared/utils/app-error-handler';
 
 //? Components
-import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { InitTableComponent } from './shared/components/init-table/init-table.component';
 import { HomeComponent } from './pages/home/home.component';
-import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { CoordinatorModalComponent } from './shared/components/coordinator-modal/coordinator-modal.component';
 import { AddCoordinatorModalComponent } from './shared/components/add-coordinator-modal/add-coordinator-modal.component';
 import { CoordinatorFilterPipe } from './shared/pipes/coordinator-filter.pipe';
@@ -30,7 +27,6 @@ import { MatSortModule } from '@angular/material/sort';
 import { MatPaginatorModule } from '@angular/material/paginator';
 import { CustomFormsModule } from './shared/components/custom-forms/custom-forms.module';
 import { DevTagComponent } from './shared/components/dev-tag/dev-tag.component';
-import { DevPanelComponent } from './shared/components/dev-panel/dev-panel.component';
 import { IbdAngularComponentsModule } from 'ibd-angular-components';
 
 //? Module of components
@@ -42,23 +38,20 @@ import { NgxHotjarRouterModule, NgxHotjarModule } from 'ngx-hotjar';
 import { environment } from '../environments/environment';
 import { CurrencyPipe } from '@angular/common';
 import { LoginModule } from './pages/login/login.module';
-import { LoginCardModule } from './shared/components/login-card/login-card.module';
 import { JwtExpirationSubscriptionModule } from './shared/components/jwt-expiration-subscription/jwt-expiration-subscription.module';
 import { FooterModule } from './shared/components/footer/footer.module';
 import { HeaderModule } from './shared/components/header/header.module';
 import { NavbarModule } from './shared/components/navbar/navbar.module';
+import { DevTagModule } from './shared/components/dev-tag/dev-tag.module';
 @NgModule({
   declarations: [
     AppComponent,
-    NotFoundComponent,
     HomeComponent,
     InitTableComponent,
     CoordinatorModalComponent,
     AddCoordinatorModalComponent,
     CoordinatorFilterPipe,
     CreateUserModalComponent,
-    DevTagComponent,
-    DevPanelComponent,
     TawkToComponent,
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
@@ -87,6 +80,7 @@ import { NavbarModule } from './shared/components/navbar/navbar.module';
     FooterModule,
     HeaderModule,
     NavbarModule,
+    DevTagModule
   ],
   providers: [
     CurrencyPipe,
