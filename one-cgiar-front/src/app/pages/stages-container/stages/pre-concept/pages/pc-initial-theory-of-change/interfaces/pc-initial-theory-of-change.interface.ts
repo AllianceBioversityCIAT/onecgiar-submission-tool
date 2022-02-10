779069daae49bf218f8cf83282a8e541fbd0e55d
Yecksin Mauricio <yecksin@gmail.com>
2022-02-10 08:31:58 -0500

    PcInitialTheoryOfChangeBody interface added

---
+++ one-cgiar-front/src/app/pages/stages-container/stages/pre-concept/pages/pc-initial-theory-of-change/interfaces/pc-initial-theory-of-change.interface.ts
@@ -0,0 +1,8 @@
+export interface PcInitialTheoryOfChangeBody{
+    id: number, 
+    narrative: string, 
+    active: boolean, 
+    section: string, 
+    updateFiles: [],
+    filesSavedListAux:[]
+}
