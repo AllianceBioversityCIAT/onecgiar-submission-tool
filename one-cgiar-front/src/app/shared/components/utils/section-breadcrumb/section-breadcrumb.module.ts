c54fed1632058fd468effd006c4ed8ba7659ae39
Yecksin Mauricio <yecksin@gmail.com>
2022-02-14 08:31:51 -0500

    section-breadcrumb.module added in utils module

---
+++ one-cgiar-front/src/app/shared/components/utils/section-breadcrumb/section-breadcrumb.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SectionBreadcrumbComponent } from './section-breadcrumb.component';
+import { RouterModule } from '@angular/router';
+import { PcWorkPackageRoutingModule } from '../../../../pages/stages-container/stages/pre-concept/pages/pc-wp-and-geo-focus/pages/pc-work-package/pc-work-package-routing.module';
+
+
+
+@NgModule({
+  declarations: [SectionBreadcrumbComponent],
+  exports:[SectionBreadcrumbComponent],
+  imports: [
+    CommonModule,
+    RouterModule,
+    PcWorkPackageRoutingModule
+  ]
+})
+export class SectionBreadcrumbModule { }
