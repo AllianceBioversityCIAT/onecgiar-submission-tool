4b2c1a5b9e7679940c44ffec545ea4e07a55bf91
Yecksin Mauricio <yecksin@gmail.com>
2022-02-15 15:49:11 -0500

    initial path "@env/" was improved

--- one-cgiar-front/src/app/shared/services/initiatives.service.ts
+++ one-cgiar-front/src/app/shared/services/initiatives.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from '@env/environment';
-import { observable, Observable } from 'rxjs';
-import { AllInitiatives } from '../models/initiative.interface';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
+import { environment } from '../../../environments/environment';
 const sectionPath = 'initiatives'
 
 @Injectable({
