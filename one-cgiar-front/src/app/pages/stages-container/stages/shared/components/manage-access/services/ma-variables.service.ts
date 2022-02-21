741f8f18b7cc9a96d138aebca69f2a282bed21ac
Yecksin Mauricio <yecksin@gmail.com>
2022-02-21 11:53:01 -0500

    save orderBox variable in local storage

--- one-cgiar-front/src/app/pages/stages-container/stages/shared/components/manage-access/services/ma-variables.service.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/shared/components/manage-access/services/ma-variables.service.ts
@@ -4,6 +4,12 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class MaVariablesService {
-  orderBox = true;
+ 
   constructor() { }
+  get orderBox(){
+    return localStorage.getItem('orderBox') == 'true' ? true : false;
+  }
+  set orderBox(value){
+    localStorage.setItem('orderBox', value.toString());
+  }
 }
