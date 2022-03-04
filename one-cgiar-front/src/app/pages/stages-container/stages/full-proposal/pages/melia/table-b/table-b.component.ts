2edfcacdbd59a615ee9083c17568c32c57676cf3
Yecksin Mauricio <yecksin@gmail.com>
2022-03-04 08:17:12 -0500

    Show data in table A and B

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/table-b/table-b.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/table-b/table-b.component.ts
@@ -8,13 +8,29 @@ import { InitiativesService } from '../../../../../../../shared/services/initiat
   styleUrls: ['./table-b.component.scss']
 })
 export class TableBComponent implements OnInit {
-
+  tableBData:TableBData
   constructor( private _initiativesService:InitiativesService) { }
 
   ngOnInit(): void {
-    this._initiativesService.getMeliaResultFramework(this._initiativesService.initiative.id).pipe(map(res=>res.response.melia.resultFramework.tableB)).subscribe(resp=>{
-      console.log(resp);
+    this._initiativesService.getMeliaResultFramework(this._initiativesService.initiative.id).pipe(map(res=>res.response.melia.resultFramework.tableB)).subscribe((resp:TableBData)=>{
+      this.tableBData = resp;
+      console.log(this.tableBData)
     })
   }
 
+}
+
+interface TableBData {
+  actionAreasOutcomesIndicators: ActionAreasOutcomesIndicator[];
+}
+
+interface ActionAreasOutcomesIndicator {
+  initvStgId: number;
+  id: number;
+  outcomes_indicators_id: number;
+  outcome_id: number;
+  outcome_statement: string;
+  outcome_indicator_id: number;
+  outcome_indicator_smo_code: string;
+  outcome_indicator_statement: string;
 }
--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/table-c/table-c.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/table-c/table-c.component.ts
@@ -8,13 +8,43 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./table-c.component.scss']
 })
 export class TableCComponent implements OnInit {
-
+  resultData: ResultData;
   constructor( private _initiativesService:InitiativesService) { }
 
   ngOnInit(): void {
-    this._initiativesService.getMeliaResultFramework(this._initiativesService.initiative.id).pipe(map(res=>res.response.melia.resultFramework.tableC)).subscribe(resp=>{
-      console.log(resp);
+    this._initiativesService.getMeliaResultFramework(this._initiativesService.initiative.id).pipe(map(res=>res.response.melia.resultFramework.tableC.results)).subscribe((resp:ResultData)=>{
+      console.log(resp.indicators[0]);
+      console.log(resp.result_title)
+      this.resultData = resp
     })
   }
 
+}
+
+
+interface ResultData {
+  initvStgId: number;
+  id: number;
+  result_type_id: number;
+  result_title: string;
+  is_global: number;
+  active: number;
+  indicators: Indicator[];
+}
+
+interface Indicator {
+  id: number;
+  name: string;
+  unit_measurement: string;
+  results_id: number;
+  baseline_value: string;
+  baseline_year: number;
+  target_value: string;
+  target_year: number;
+  active: number;
+  data_source: string;
+  data_collection_method: string;
+  frequency_data_collection: string;
+  created_at: string;
+  updated_at: string;
 }
