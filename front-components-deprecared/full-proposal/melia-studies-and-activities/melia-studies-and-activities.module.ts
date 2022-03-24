c8ed5dba9943c1fe2f715280f1c87a92b4c2f5bc
Yecksin Mauricio <yecksin@gmail.com>
2022-03-24 11:17:04 -0500

    Backup of melia-studies-and-activities ( save files)

---
+++ front-components-deprecared/full-proposal/melia-studies-and-activities/melia-studies-and-activities.module.ts
@@ -0,0 +1,23 @@
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { MeliaStudiesAndActivitiesRoutingModule } from './melia-studies-and-activities-routing.module';
+import { MeliaStudiesAndActivitiesComponent } from './melia-studies-and-activities.component';
+import { UtilsModule } from '../../../../../../../shared/components/utils/utils.module';
+// import { IbdAngularComponentsModule } from '../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [MeliaStudiesAndActivitiesComponent],
+  imports: [
+    CommonModule,
+    MeliaStudiesAndActivitiesRoutingModule,
+    UtilsModule,
+    IbdAngularComponentsModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class MeliaStudiesAndActivitiesModule { }
