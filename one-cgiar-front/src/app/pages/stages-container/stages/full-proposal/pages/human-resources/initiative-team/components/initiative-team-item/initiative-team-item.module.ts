f631d0aafe756939161419d48f657d9b6977c62c
Yecksin Mauricio <yecksin@gmail.com>
2022-04-06 16:08:14 -0500

    IbdAngularComponentsModule import fixed

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/human-resources/initiative-team/components/initiative-team-item/initiative-team-item.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/human-resources/initiative-team/components/initiative-team-item/initiative-team-item.module.ts
@@ -1,8 +1,8 @@
 import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { InitiativeTeamItemComponent } from './initiative-team-item.component';
-// import { IbdAngularComponentsModule } from 'ibd-angular-components';
-import { IbdAngularComponentsModule } from '../../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+import { IbdAngularComponentsModule } from 'ibd-angular-components';
+// import { IbdAngularComponentsModule } from '../../../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
 import { FormsModule } from '@angular/forms';
 import { ButtonEditOrDeleteModule } from '../../../../../../shared/components/button-edit-or-delete/button-edit-or-delete.module';
 
