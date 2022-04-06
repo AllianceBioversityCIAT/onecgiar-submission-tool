e9d27b88dd71bf7132493ec6a4f0b00d9fb52320
Yecksin Mauricio <yecksin@gmail.com>
2022-04-06 09:34:38 -0500

    Ready only fields added in all sections

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/human-resources/initiative-team/initiative-team.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/human-resources/initiative-team/initiative-team.module.ts
@@ -3,8 +3,8 @@ import { CommonModule } from '@angular/common';
 
 import { InitiativeTeamRoutingModule } from './initiative-team-routing.module';
 import { InitiativeTeamComponent } from './initiative-team.component';
-import { IbdAngularComponentsModule } from 'ibd-angular-components';
-// import { IbdAngularComponentsModule } from '../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
+// import { IbdAngularComponentsModule } from 'ibd-angular-components';
+import { IbdAngularComponentsModule } from '../../../../../../../../../../../ibd-angular-library/projects/ibd-angular-components/src/lib/ibd-angular-components.module';
 import { UtilsModule } from '../../../../../../../shared/components/utils/utils.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { InitiativeTeamItemModule } from './components/initiative-team-item/initiative-team-item.module';
