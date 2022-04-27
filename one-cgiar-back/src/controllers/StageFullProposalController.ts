84a771fe9775732cdc9fe58d359899b28fb7c821
JuanCode <46978099+JuankCadavid@users.noreply.github.com>
2022-04-27 17:57:17 -0500

    JC - add stageId parameter for all routes in  full proposal

--- one-cgiar-back/src/controllers/StageFullProposalController.ts
+++ one-cgiar-back/src/controllers/StageFullProposalController.ts
@@ -23,13 +23,13 @@ import {WorkPackages} from '../entity/WorkPackages';
 
 export const getGeneralInformation = async (req: Request, res: Response) => {
   // get initiative by stage id from client
-  const {stageId,initiativeId} = req.params;
+  const {stageId, initiativeId} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {id:stageId}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -79,7 +79,7 @@ export const getGeneralInformation = async (req: Request, res: Response) => {
  * @returns
  */
 export async function getWorkPackages(req: Request, res: Response) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
 
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
@@ -87,7 +87,7 @@ export async function getWorkPackages(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -194,7 +194,7 @@ export async function getAllWorkPackages(req: Request, res: Response) {
  * @returns
  */
 export async function patchWorkPackage(req: Request, res: Response) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
   const {
     acronym,
     name,
@@ -221,7 +221,7 @@ export async function patchWorkPackage(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -269,7 +269,7 @@ export async function patchWorkPackage(req: Request, res: Response) {
  */
 export const upsertGeneralInformation = async (req: Request, res: Response) => {
   // get initiative by stage id from client
-  const {stageId,initiativeId} = req.params;
+  const {stageId, initiativeId} = req.params;
   // get generalInformationId, name, action_area_id, action_area_description by stage id from client
   const {
     generalInformationId,
@@ -284,7 +284,7 @@ export const upsertGeneralInformation = async (req: Request, res: Response) => {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {id:stageId}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -337,13 +337,13 @@ export const upsertGeneralInformation = async (req: Request, res: Response) => {
  */
 export const getContext = async (req: Request, res: Response) => {
   // get initiative by stage id from client
-  const {stageId,initiativeId} = req.params;
+  const {stageId, initiativeId} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {id:stageId}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -391,7 +391,7 @@ export const getContext = async (req: Request, res: Response) => {
 
 export const upsertContext = async (req: Request, res: Response, next) => {
   // get initiative by stage id from client
-  const {stageId,initiativeId} = req.params;
+  const {stageId, initiativeId} = req.params;
   // get generalInformationId, name, action_area_id, action_area_description by stage id from client
   const {
     contextId,
@@ -409,7 +409,7 @@ export const upsertContext = async (req: Request, res: Response, next) => {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {id:stageId}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -464,7 +464,7 @@ export const upsertContext = async (req: Request, res: Response, next) => {
  * @returns { projectionBenefits }
  */
 export async function patchProjectionBenefits(req: Request, res: Response) {
-  const {stageId,initiativeId} = req.params;
+  const {stageId, initiativeId} = req.params;
 
   // projection benefits section data
   const {
@@ -489,7 +489,7 @@ export async function patchProjectionBenefits(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {id:stageId}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -542,14 +542,14 @@ export async function patchProjectionBenefits(req: Request, res: Response) {
  * @returns { projectionBenefits }
  */
 export async function getProjectionBenefits(req: Request, res: Response) {
-  const {stageId,initiativeId} = req.params;
+  const {stageId, initiativeId} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {id:stageId}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -591,14 +591,14 @@ export async function getProjectionBenefitsByImpact(
   req: Request,
   res: Response
 ) {
-  const {stageId,initiativeId, impactId} = req.params;
+  const {stageId, initiativeId, impactId} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {id:stageId}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -639,7 +639,7 @@ export async function getProjectionBenefitsByImpact(
  * @returns { impactStrategies }
  */
 export async function patchImpactStrategies(req: Request, res: Response) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
 
   // impact strategies section data
   const {
@@ -661,7 +661,7 @@ export async function patchImpactStrategies(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -711,14 +711,14 @@ export async function patchImpactStrategies(req: Request, res: Response) {
  * @returns { impactStrategies }
  */
 export async function getImpactStrategies(req: Request, res: Response) {
-  const {initiativeId, impactAreaId} = req.params;
+  const {stageId,initiativeId, impactAreaId} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -759,7 +759,7 @@ export async function getImpactStrategies(req: Request, res: Response) {
  * @returns melia
  */
 export async function patchMeliaAndFiles(req: Request, res: Response) {
-  const {initiativeId, ubication} = req.params;
+  const {stageId,initiativeId, ubication} = req.params;
 
   //melia section data
   // const {id, melia_plan, active, section, updateFiles, tableA, tableB, tableC} =
@@ -778,7 +778,7 @@ export async function patchMeliaAndFiles(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -830,14 +830,14 @@ export async function patchMeliaAndFiles(req: Request, res: Response) {
  * @returns meliaData
  */
 export async function getMeliaAndFiles(req: Request, res: Response) {
-  const {initiativeId, sectionName} = req.params;
+  const {stageId,initiativeId, sectionName} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -874,7 +874,7 @@ export async function patchMeliaStudiesActivities(
   req: Request,
   res: Response
 ): Promise<Response> {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
   const meliaStudiesActivitiesData = req.body;
 
   const initvStgRepo = getRepository(InitiativesByStages);
@@ -883,7 +883,7 @@ export async function patchMeliaStudiesActivities(
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -926,7 +926,7 @@ export async function getMeliaStudiesActivities(
   req: Request,
   res: Response
 ): Promise<Response> {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
 
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
@@ -934,7 +934,7 @@ export async function getMeliaStudiesActivities(
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -974,7 +974,7 @@ export async function getMeliaStudiesActivities(
  * @returns managePlanRisk
  */
 export async function patchManagePlanAndFiles(req: Request, res: Response) {
-  const {initiativeId, ubication} = req.params;
+  const {stageId,initiativeId, ubication} = req.params;
 
   //melia section data
   const {id, management_plan, active, section, updateFiles, riskassessment} =
@@ -989,7 +989,7 @@ export async function patchManagePlanAndFiles(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1043,14 +1043,14 @@ export async function patchManagePlanAndFiles(req: Request, res: Response) {
  * @returns managePlanData
  */
 export async function getManagePlanAndFiles(req: Request, res: Response) {
-  const {initiativeId, sectionName} = req.params;
+  const {stageId,initiativeId, sectionName} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1089,7 +1089,7 @@ export async function getManagePlanAndFiles(req: Request, res: Response) {
  * @returns humanResources
  */
 export async function patchHumanResourcesAndFiles(req: Request, res: Response) {
-  const {initiativeId, ubication} = req.params;
+  const {stageId,initiativeId, ubication} = req.params;
 
   //melia section data
   const {
@@ -1110,7 +1110,7 @@ export async function patchHumanResourcesAndFiles(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1166,14 +1166,14 @@ export async function patchHumanResourcesAndFiles(req: Request, res: Response) {
  * @returns humanResourcesData
  */
 export async function getHumanResources(req: Request, res: Response) {
-  const {initiativeId, sectionName} = req.params;
+  const {stageId,initiativeId, sectionName} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1214,7 +1214,7 @@ export async function getHumanResources(req: Request, res: Response) {
  * @returns financialResources
  */
 export async function patchFinancialResources(req: Request, res: Response) {
-  const {initiativeId, sectionName} = req.params;
+  const {stageId,initiativeId, sectionName} = req.params;
 
   //financial resources section data
   const fResources = req.body;
@@ -1229,7 +1229,7 @@ export async function patchFinancialResources(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1301,14 +1301,14 @@ export async function patchFinancialResources(req: Request, res: Response) {
  * @returns financialResourcesData
  */
 export async function getFinancialResources(req: Request, res: Response) {
-  const {initiativeId, sectionName} = req.params;
+  const {stageId,initiativeId, sectionName} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1327,9 +1327,8 @@ export async function getFinancialResources(req: Request, res: Response) {
     // create new full proposal object
     const fullPposal = new ProposalHandler(initvStg.id.toString());
 
-    const financialResourcesData = await fullPposal.requestFinancialResourcesBySection(
-      sectionName
-    );
+    const financialResourcesData =
+      await fullPposal.requestFinancialResourcesBySection(sectionName);
 
     res.json(
       new ResponseHandler('Full Proposal:financial resources.', {
@@ -1367,7 +1366,7 @@ export async function patchPolicyComplianceOversight(
   req: Request,
   res: Response
 ) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
 
   //Policy compliance Oversight section data
   const {
@@ -1384,7 +1383,7 @@ export async function patchPolicyComplianceOversight(
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1433,14 +1432,14 @@ export async function getPolicyComplianceOversight(
   req: Request,
   res: Response
 ) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1480,7 +1479,7 @@ export async function getPolicyComplianceOversight(
  * @returns
  */
 export async function patchInnovationPackages(req: Request, res: Response) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
 
   //Policy compliance Oversight section data
   const {id, key_principles, active} = req.body;
@@ -1491,7 +1490,7 @@ export async function patchInnovationPackages(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1534,14 +1533,14 @@ export async function patchInnovationPackages(req: Request, res: Response) {
  * @returns
  */
 export async function getInnovationPackages(req: Request, res: Response) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
   const initvStgRepo = getRepository(InitiativesByStages);
   const stageRepo = getRepository(Stages);
 
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1580,7 +1579,7 @@ export async function getInnovationPackages(req: Request, res: Response) {
  * @returns tocs
  */
 export async function patchTocs(req: Request, res: Response) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
   const toc = req.body;
 
   //Validate stage
@@ -1590,7 +1589,7 @@ export async function patchTocs(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
@@ -1628,7 +1627,7 @@ export async function patchTocs(req: Request, res: Response) {
  * @returns tocs
  */
 export async function getTocByInitiative(req: Request, res: Response) {
-  const {initiativeId} = req.params;
+  const {stageId,initiativeId} = req.params;
   const toc = req.body;
 
   //Validate stage
@@ -1638,7 +1637,7 @@ export async function getTocByInitiative(req: Request, res: Response) {
   try {
     // get stage
     const stage = await stageRepo.findOne({
-      where: {description: 'Full Proposal'}
+      where: {id: stageId}
     });
     // get intiative by stage : proposal
     const initvStg: InitiativesByStages = await initvStgRepo.findOne({
