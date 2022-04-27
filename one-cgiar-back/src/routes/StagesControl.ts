7e39b709fd64120654dd022bc5fa6cce6a4ef06e
JuanCode <46978099+JuankCadavid@users.noreply.github.com>
2022-04-27 15:09:28 -0500

    Update StagesControl.ts

--- one-cgiar-back/src/routes/StagesControl.ts
+++ one-cgiar-back/src/routes/StagesControl.ts
@@ -7,6 +7,6 @@ const router = Router();
 // concept routes
 router.use('/pre-concept', preConcept);
 // full proposal routes
-router.use('/full-proposal', proposal);
+router.use('/proposal', proposal);
 
 export default router;
