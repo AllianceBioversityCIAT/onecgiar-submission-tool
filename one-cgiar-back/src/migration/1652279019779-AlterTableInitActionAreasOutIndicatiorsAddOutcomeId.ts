bd797992b8cf558ca393cc5e7035d768a77ac299
JuanCode <46978099+JuankCadavid@users.noreply.github.com>
2022-05-11 09:52:27 -0500

    JC - Improve uptdate for all entities and add outcome id for table B

---
+++ one-cgiar-back/src/migration/1652279019779-AlterTableInitActionAreasOutIndicatiorsAddOutcomeId.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AlterTableInitActionAreasOutIndicatiorsAddOutcomeId1652279019779 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`
+        ALTER TABLE init_action_areas_out_indicators ADD COLUMN outcome_id int(11) DEFAULT NULL
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
