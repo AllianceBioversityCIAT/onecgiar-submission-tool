8487022433d079bb90a04f51b3e2108bc7ef532a
Santiago Gálvez <santiagogalvez17@gmail.com>
2022-05-20 15:34:21 -0500

    Create Years table migration

---
+++ one-cgiar-back/src/entity/Years.ts
@@ -0,0 +1,10 @@
+import { IsNotEmpty } from "class-validator";
+import { Column, Entity, PrimaryColumn } from "typeorm";
+import { UpdatedCreatedAt } from "./extends/UpdateCreateAt";
+
+@Entity('years')
+export class Years extends UpdatedCreatedAt{
+    @PrimaryColumn({length: '4'})
+    @IsNotEmpty()
+    year: string;
+}
