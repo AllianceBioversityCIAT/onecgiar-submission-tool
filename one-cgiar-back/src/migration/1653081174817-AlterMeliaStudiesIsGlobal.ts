49ffda02ff84f7d42f888f626f021febe97c249e
Santiago Gálvez <santiagogalvez17@gmail.com>
2022-05-20 16:39:39 -0500

    Add is_global and CountriesByMeliaStd table

---
+++ one-cgiar-back/src/migration/1653081174817-AlterMeliaStudiesIsGlobal.ts
@@ -0,0 +1,14 @@
+import {MigrationInterface, QueryRunner} from "typeorm";
+
+export class AlterMeliaStudiesIsGlobal1653081174817 implements MigrationInterface {
+    name = 'AlterMeliaStudiesIsGlobal1653081174817'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query("ALTER TABLE `melia_studies_activities` ADD `is_global` tinyint NULL DEFAULT NULL");
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        
+    }
+
+}
