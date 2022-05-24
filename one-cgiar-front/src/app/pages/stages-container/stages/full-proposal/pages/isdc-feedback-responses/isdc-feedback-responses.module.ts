c4fcf55afb84c4c2c8b4d79bd4369ffe648ac034
davinzifc <47566127+davinzifc@users.noreply.github.com>
2022-05-24 16:22:12 -0500

    changed the location:  recommendations table

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/isdc-feedback-responses/isdc-feedback-responses.module.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/isdc-feedback-responses/isdc-feedback-responses.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import { NgxSpinnerModule } from 'ngx-spinner';
+
 import { IsdcFeedbackResponsesRoutingModule } from './isdc-feedback-responses-routing.module';
 import { IsdcFeedbackResponsesComponent } from './isdc-feedback-responses.component';
 import { UtilsModule } from '../../../../../../shared/components/utils/utils.module';
 import { IbdAngularComponentsModule } from 'ibd-angular-components';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import { AddButtonModule } from '../../../../../../shared/components/add-button/add-button.module';
+import { ButtonEditOrDeleteModule } from '../../../shared/components/button-edit-or-delete/button-edit-or-delete.module';
+import { CompactInformationTableViewModule } from '../../../../../../shared/components/compact-information-table-view/compact-information-table-view.module';
+import { CollapsibleContainerModule } from '../../../../../../shared/components/collapsible-container/collapsible-container.module';
 
 @NgModule({
   declarations: [IsdcFeedbackResponsesComponent],
@@ -16,7 +21,12 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     UtilsModule,
     IbdAngularComponentsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    NgxSpinnerModule,
+    AddButtonModule,
+    ButtonEditOrDeleteModule,
+    CompactInformationTableViewModule,
+    CollapsibleContainerModule
   ]
 })
 export class IsdcFeedbackResponsesModule { }
