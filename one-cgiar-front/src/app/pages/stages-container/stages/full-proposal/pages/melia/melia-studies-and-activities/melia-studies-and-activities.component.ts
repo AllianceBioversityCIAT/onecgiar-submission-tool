4484d8f991f38171a03c30250a82570e1c0336be
Santiago Gálvez <santiagogalvez17@gmail.com>
2022-05-26 15:53:18 -0500

    Update geographic scope txt on MeliaStd table

--- one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/melia-studies-and-activities/melia-studies-and-activities.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages/full-proposal/pages/melia/melia-studies-and-activities/melia-studies-and-activities.component.ts
@@ -172,9 +172,9 @@ export class MeliaStudiesAndActivitiesComponent implements OnInit {
   formatGeographicScope(arrayMelias: MeliaStudiesAndActivities[]) {
     for (const melia of arrayMelias) {
       melia['geographic_scope'] = `
-      <p><strong>Is global: </strong>${melia.is_global ? 'Yes' : 'No'}</p>
-      ${melia.countries.length ? `<p><strong>Countries: </strong>${melia.countries.map((coun:any) => coun.name).join(', ')}</p>` : ''}
+      <p><strong>Global scope: </strong>${melia.is_global ? 'Yes' : 'No'}</p>
       ${melia.regions.length ? `<p><strong>Regions: </strong>${melia.regions.map((coun:any) => coun.name).join(', ')}</p>` : ''}
+      ${melia.countries.length ? `<p><strong>Countries: </strong>${melia.countries.map((coun:any) => coun.name).join(', ')}</p>` : ''}
       `
     }
   }
