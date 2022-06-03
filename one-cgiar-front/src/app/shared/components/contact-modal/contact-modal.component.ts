f64ca4907fd727465da98c6ded65e6e92e39055a
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2022-06-03 10:37:53 -0500

    Contact modal component added

---
+++ one-cgiar-front/src/app/shared/components/contact-modal/contact-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-contact-modal',
+  templateUrl: './contact-modal.component.html',
+  styleUrls: ['./contact-modal.component.scss']
+})
+export class ContactModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
