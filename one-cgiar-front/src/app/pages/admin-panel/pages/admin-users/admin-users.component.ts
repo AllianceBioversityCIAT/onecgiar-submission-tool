fe14c7738fb9755f8dd63e5bd9208166174d8f62
davinzifc <47566127+davinzifc@users.noreply.github.com>
2022-06-15 09:47:33 -0500

    udate all reporting

--- one-cgiar-front/src/app/pages/admin-panel/pages/admin-users/admin-users.component.ts
+++ one-cgiar-front/src/app/pages/admin-panel/pages/admin-users/admin-users.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Table } from 'primeng/table';
 import { InitiativesService } from '../../../../shared/services/initiatives.service';
+import { ManageExcelService } from '../../../stages-container/stages/full-proposal/services/manage-excel.service';
 
 @Component({
   selector: 'app-admin-users',
@@ -10,7 +11,9 @@ import { InitiativesService } from '../../../../shared/services/initiatives.serv
 export class AdminUsersComponent implements OnInit {
 
   usersList = [];
-  constructor( private _interactionsService:InitiativesService) { }
+  constructor( 
+    private _manageExcelService:ManageExcelService,
+    private _interactionsService:InitiativesService) { }
 
   ngOnInit(): void {
     this.getUsersWithInitiativesInformation();
@@ -19,13 +22,32 @@ export class AdminUsersComponent implements OnInit {
   getUsersWithInitiativesInformation(){
     this._interactionsService.getUsersWithInitiativesInformation().subscribe(resp=>{
       this.usersList = resp.data;
-      this.usersList.map(user=>{user.name = user.first_name + ' '+ user.last_name})
-      console.log(this.usersList)
     })
   }
 
   clear(table: Table) {
     table.clear();
+}
+
+exportExcel() {
+
+  import("xlsx").then(xlsx => {
+    const worksheet = xlsx.utils.json_to_sheet(this.usersList);
+    var wscols = [
+      {wpx:70},
+      {wpx:300},
+      {wpx:100},
+      {wpx:100},
+      {wpx:100},
+      {wpx:100}
+  ];
+  
+  worksheet['!cols'] = wscols;
+    const workbook = { Sheets: { 'data': worksheet }, SheetNames: ['data'] };
+    const excelBuffer: any = xlsx.write(workbook, { bookType: 'xlsx', type: 'array' });
+
+    this._manageExcelService.saveAsExcelFile(excelBuffer, `ISDC Comments status`);
+  });
 }
 
 }
