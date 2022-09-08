c9f175094d848bba9815d54521cf77aabac3200f
yecksin <yecksin@gmail.com>
2022-09-08 11:30:50 -0500

    :recycle: refactor(Environments): biUrl added as environment variable

--- one-cgiar-front/src/app/pages/bi/bi.component.ts
+++ one-cgiar-front/src/app/pages/bi/bi.component.ts
@@ -1,17 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { InitiativesService } from '../../shared/services/initiatives.service';
+import { environment } from '../../../environments/environment';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-bi',
   templateUrl: './bi.component.html',
-  styleUrls: ['./bi.component.scss']
+  styleUrls: ['./bi.component.scss'],
 })
 export class BiComponent implements OnInit {
-
-  constructor( private _initiativesService:InitiativesService) { }
+  constructor(
+    private _initiativesService: InitiativesService,
+    private _sanitizer: DomSanitizer
+  ) {}
 
   ngOnInit(): void {
-    this._initiativesService.setTitle('BI'); 
+    this._initiativesService.setTitle('BI');
   }
 
+  saniUrl() {
+    return this._sanitizer.bypassSecurityTrustResourceUrl(environment.biUrl);
+  }
 }
