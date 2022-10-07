ed96ebc8814fa28f3301f1c66f687f1ef95cad2e
davinzifc <47566127+davinzifc@users.noreply.github.com>
2022-10-07 09:11:49 -0500

    :recycle: refactor(packages): add init name in the end-point

--- one-cgiar-back/src/handlers/FullProposalDomain.ts
+++ one-cgiar-back/src/handlers/FullProposalDomain.ts
@@ -279,13 +279,14 @@ export class ProposalHandler extends InitiativeStageHandler {
                 GROUP BY id,region_id
                 `,
         WPquery = `
-        SELECT init.initiativeId as initiative_id,init.stageId as stage_id,
+        SELECT init.initiativeId as initiative_id,i.name as init_name, init.stageId as stage_id, 
           wp.id as wp_id, wp.active, wp.name, wp.results, wp.pathway_content, 
           wp.is_global, wp.initvStgId, wp.created_at, wp.updated_at, wp.acronym,
           wp.wp_official_code 
          FROM work_packages wp
          JOIN initiatives_by_stages init
            on wp.initvStgId  = init.id
+         inner join initiatives i on i.id = init.initiativeId 
         WHERE init.stageId = 3
          AND wp.active = 1
         ORDER BY initiativeId asc;`;
