8b9f5211c7a2e2f9acfb8b271fe54eaeeaa7899d
jdmunozg <juan.david.m.g1004@gmail.com>
2023-02-02 15:49:39 -0500

    feat(Create post parameters)

--- one-cgiar-back/src/routes/TocRoutes.ts
+++ one-cgiar-back/src/routes/TocRoutes.ts
@@ -4,6 +4,6 @@ import * as TocResultDashboard from '../toc/controllers/TocControllerResult';
 const router = Router();
 
 // get information toc result dashboard
-router.get('/toc', TocResultDashboard.getTocResultDashboard);
+router.post('/toc', TocResultDashboard.getTocResultDashboard);
 
 export default router;
--- one-cgiar-back/src/toc/controllers/TocControllerResult.ts
+++ one-cgiar-back/src/toc/controllers/TocControllerResult.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import {Request, Response} from 'express';
 import {TocServicesResults} from '../services/TocServicesResult'
-const tocHost = 'https://toc.loc.codeobia.com/api/toc/16c17bd0-0d40-423f-aa90-6572034d95da/dashboard-result';
+
 
 /**
  *GET TOC Result Dashboard
@@ -9,7 +9,11 @@ const tocHost = 'https://toc.loc.codeobia.com/api/toc/16c17bd0-0d40-423f-aa90-65
  */
 
 export async function getTocResultDashboard(req: Request,res: Response) {
-    try {
+  
+  const id_toc = req.body.id_toc;
+  let tocHost = 'https://toc.loc.codeobia.com/api/toc/'+id_toc+'/dashboard-result';
+  
+    try {      
       let servicesInformation = new TocServicesResults();
         const narrative = await axios.get(tocHost);
           const message = await servicesInformation.splitInformation(narrative.data)
@@ -18,7 +22,7 @@ export async function getTocResultDashboard(req: Request,res: Response) {
       } catch (error) {
         
         return {
-          error:'Not exists information'
+          error:error
         };
       }
 }
