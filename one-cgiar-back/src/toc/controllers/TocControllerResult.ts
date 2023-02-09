9ab71445cf33c9083dd85dc791590743f3da2e28
jdmunozg <juan.david.m.g1004@gmail.com>
2023-02-09 09:27:32 -0500

    refactor(Changes endpoint toc)

--- one-cgiar-back/src/toc/controllers/TocControllerResult.ts
+++ one-cgiar-back/src/toc/controllers/TocControllerResult.ts
@@ -13,7 +13,7 @@ export async function getTocResultDashboard(req: Request,res: Response) {
   
   const id_toc = await req.body.id_toc;
   
-  let tocHost = await 'https://toc.mel.cgiar.org/api/toc/048e886f-0970-49cf-9793-8e3f25366b9e/dashboard-result';
+  let tocHost = await 'https://toc.mel.cgiar.org/api/toc/'+id_toc+'/dashboard-result';
     console.log(tocHost);
     try {      
       let servicesInformation = new TocServicesResults();
