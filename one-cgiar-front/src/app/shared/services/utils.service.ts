522276b805a391adf31fc57b5624a77ca12c396b
JuanCode <jucacar28@hotmail.com>
2023-11-28 08:09:06 -0500

    Revert "Merge pull request #127 from AllianceBioversityCIAT/staging"
    This reverts commit 30a358f927c5ad46909cd52603fe38d74e66980e, reversing
    changes made to 701ebed585f5e7112998a5fdb8712c4977d5b8d6.

--- one-cgiar-front/src/app/shared/services/utils.service.ts
+++ one-cgiar-front/src/app/shared/services/utils.service.ts
@@ -10,48 +10,51 @@ export class UtilsService {
   private sidebarOpened = new BehaviorSubject<boolean>(false);
   sidebarOpened$ = this.sidebarOpened.asObservable();
 
-  constructor(private router: Router, private _initiativesService: InitiativesService, private _authService: AuthService, private _interactionsService: InteractionsService) {}
+  constructor(
+    private router:Router, 
+    private _initiativesService:InitiativesService,
+    private _authService:AuthService
+  ){}
 
   openSidebar(value: boolean): void {
     this.sidebarOpened.next(value);
   }
 
-  convertToKebabCase(text: string) {
+  convertToKebabCase(text:string){
     return text.split(' ').join('-').toLowerCase();
   }
 
-  goToEditToc() {
-    // if(this._initiativesService.initiative.status == 'Approved' && this._authService.lsUserRoles.id != 1) return;
-    this._initiativesService
-      .getProposalTocByInitiativeId()
-      .pipe(map(res => res?.response?.fullInitiativeToc?.toc_id))
-      .subscribe(toc_id => {
-        console.log(toc_id);
-        console.log(this._authService?.userValue?.id);
-        this._initiativesService.authTocToken(this._authService?.userValue?.id).subscribe(
-          token => {
-            // window.open(`${environment.tocUrl}?token=${token}&toc_id=${toc_id}`,"_blank");
-            var w = window.innerWidth - window.innerWidth / 3;
-            var h = window.innerHeight - window.innerHeight / 3;
-            window.open(`${environment.tocUrl}?token=${token}&toc_id=${toc_id}`, 'winname', `directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=${w},height=${h}`);
-          },
-          err => {
-            console.log(err);
-          }
-        );
-      });
+  goToEditToc(){
+
+    this._initiativesService.getProposalTocByInitiativeId().pipe(map(res=> res?.response?.fullInitiativeToc?.toc_id)).subscribe((toc_id) => {
+      console.log(toc_id)
+      console.log(this._authService?.userValue?.id)
+      this._initiativesService.authTocToken(this._authService?.userValue?.id).subscribe(token=>{
+        // window.open(`${environment.tocUrl}?token=${token}&toc_id=${toc_id}`,"_blank");
+        var w = window.innerWidth - window.innerWidth/3;
+        var h = window.innerHeight - window.innerHeight/3;
+        window.open(`${environment.tocUrl}?token=${token}&toc_id=${toc_id}`,'winname',`directories=no,titlebar=no,toolbar=no,location=no,status=no,menubar=no,scrollbars=no,resizable=no,width=${w},height=${h}`);
+      },err=>{
+        console.log(err)
+      })
+
+
+    })
+
+
   }
 
-  get stageBaseRoute() {
-    return this.router.url.substring(0, this.router.url.lastIndexOf('stages') + 7);
+  get stageBaseRoute(){
+    return this.router.url.substring(0,this.router.url.lastIndexOf('stages')+7)
   }
 
-  validateCurrrentSection(route) {
+  validateCurrrentSection(route){    
     if (!this.router.url.includes(route)) return false;
-    return true;
+    return true
   }
 
-  navigate(route: string) {
-    this.router.navigate([route]);
+  navigate(route:string){
+    this.router.navigate([route])
   }
+
 }
