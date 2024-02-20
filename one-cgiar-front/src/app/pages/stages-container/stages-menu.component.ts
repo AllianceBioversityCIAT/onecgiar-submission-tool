b15cb24aa2aab3964350fb3f5cdd83e2124bab85
Yecksin Mauricio <53352977+yecksin@users.noreply.github.com>
2024-02-20 16:02:52 -0500

    :recycle: refactor(general-information) P2-332: hide field and change text by initiative type

--- one-cgiar-front/src/app/pages/stages-container/stages-menu.component.ts
+++ one-cgiar-front/src/app/pages/stages-container/stages-menu.component.ts
@@ -1,10 +1,5 @@
 import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import {
-  ActivatedRoute,
-  NavigationStart,
-  Router,
-  Event as NavigationEvent,
-} from '@angular/router';
+import { ActivatedRoute, NavigationStart, Router, Event as NavigationEvent } from '@angular/router';
 
 import { MatDialog } from '@angular/material/dialog';
 import { DataControlService } from '../../shared/services/data-control.service';
@@ -23,36 +18,23 @@ import { element } from 'protractor';
 @Component({
   selector: 'app-stages-menu',
   templateUrl: './stages-menu.component.html',
-  styleUrls: ['./stages-menu.component.scss'],
+  styleUrls: ['./stages-menu.component.scss']
 })
 export class StagesMenuComponent implements OnInit {
   private user = JSON.parse(localStorage.getItem('user')) || null;
   roleIdLoaded = false;
   toolTipText: string;
 
-  constructor(
-    public activatedRoute: ActivatedRoute,
-    private cdRef: ChangeDetectorRef,
-    public datepipe: DatePipe,
-    public initiativesSvc: InitiativesService,
-    public stageMenu: StagesMenuService,
-    public _interactionsService: InteractionsService,
-    public dialog: MatDialog,
-    private router: Router,
-    public _dataControlService: DataControlService,
-    private _utilsService: UtilsService,
-    public _pusherService: PusherService,
-    public _authService: AuthService
-  ) {}
+  constructor(public activatedRoute: ActivatedRoute, private cdRef: ChangeDetectorRef, public datepipe: DatePipe, public initiativesSvc: InitiativesService, public stageMenu: StagesMenuService, public _interactionsService: InteractionsService, public dialog: MatDialog, private router: Router, public _dataControlService: DataControlService, private _utilsService: UtilsService, public _pusherService: PusherService, public _authService: AuthService) {}
 
   openDialog(): void {
     const dialogRef = this.dialog.open(ManageAccessComponent, {
       width: '100%',
       height: '90%',
-      panelClass: 'custom-dialog-container',
+      panelClass: 'custom-dialog-container'
     });
 
-    dialogRef.afterClosed().subscribe((result) => {
+    dialogRef.afterClosed().subscribe(result => {
       this._dataControlService.generalInfoChange$.emit();
     });
   }
@@ -61,19 +43,12 @@ export class StagesMenuComponent implements OnInit {
 
   ngOnInit(): void {
     // console.log(this._authService.lsUserRoles.name)
-    this.sectionsList = this.router.routerState.snapshot.url
-      .substring(this.router.routerState.snapshot.url.indexOf('stages/'))
-      .split('/');
+    this.sectionsList = this.router.routerState.snapshot.url.substring(this.router.routerState.snapshot.url.indexOf('stages/')).split('/');
     let testi = 1;
     this.router.events.subscribe((event: NavigationEvent) => {
       if (event instanceof NavigationStart) {
-        this.sectionsList = event.url
-          .substring(event.url.indexOf('stages/'))
-          .split('/');
-        this._dataControlService.breadcrumbItemTwo =
-          event?.url.indexOf('work-package') !== -1
-            ? this._dataControlService.breadcrumbItemTwo
-            : '';
+        this.sectionsList = event.url.substring(event.url.indexOf('stages/')).split('/');
+        this._dataControlService.breadcrumbItemTwo = event?.url.indexOf('work-package') !== -1 ? this._dataControlService.breadcrumbItemTwo : '';
       }
     });
 
@@ -83,17 +58,14 @@ export class StagesMenuComponent implements OnInit {
 
     // TODO stageId 3 condition
     if (this.initiativesSvc.initiative.stageId === 3) {
-      this.initiativesSvc.getInitvStgId().subscribe((resp) => {
+      this.initiativesSvc.getInitvStgId().subscribe(resp => {
         this.initiativesSvc.initvStgId = resp.response;
       });
     }
 
     // TODO stageId 3 condition
-    this._dataControlService.validateMenu$.subscribe((resp) => {
-      // console.log(resp)
-      // if (this.initiativesSvc.initiative.stageId === 3) {
+    this._dataControlService.validateMenu$.subscribe(resp => {
       this.validateAllSections();
-      // }
     });
     this._dataControlService.loadMenu$.emit('full-proposal');
   }
@@ -103,41 +75,26 @@ export class StagesMenuComponent implements OnInit {
     // console.log(this.initiativesSvc.initiative.exactStageName)
     let currentRoute = this.router.url;
     if (this.currentStageAux != stageName && this.currentStageAux != '') {
-      this.router
-        .navigateByUrl(
-          `/initiatives/${this.initiativesSvc.initiative.id}/stages/${this.initiativesSvc.initiative.exactStageName}`
-        )
-        .then(() => {
-          setTimeout(() => {
-            this.router.navigateByUrl(currentRoute);
-          }, 500);
-        });
+      this.router.navigateByUrl(`/initiatives/${this.initiativesSvc.initiative.id}/stages/${this.initiativesSvc.initiative.exactStageName}`).then(() => {
+        setTimeout(() => {
+          this.router.navigateByUrl(currentRoute);
+        }, 500);
+      });
     }
     this.currentStageAux = stageName;
   }
 
   activedRouteEvent(stagesList: []) {
-    this.activatedRoute.params.subscribe((resp) => {
+    this.activatedRoute.params.subscribe(resp => {
       this.validateCurrentStageChange(resp?.stageName);
 
       stagesList.map((stageItem: any) => {
-        stageItem.stageNameKebabCase = this._utilsService.convertToKebabCase(
-          stageItem?.description
-        );
+        stageItem.stageNameKebabCase = this._utilsService.convertToKebabCase(stageItem?.description);
       });
 
-      let currentStage: any = stagesList.find(
-        (stageItem: any) => stageItem?.stageNameKebabCase == resp?.stageName
-      );
-      // console.log(currentStage)
-      // console.log(currentStage?.id)
-      // console.log("Change initiative=> " , currentStage?.id);
-      this.initiativesSvc.initiative.stageName =
-        currentStage?.stageNameKebabCase != 'pre-concept'
-          ? 'proposal'
-          : currentStage.stageNameKebabCase;
-      this.initiativesSvc.initiative.exactStageName =
-        currentStage?.stageNameKebabCase;
+      let currentStage: any = stagesList.find((stageItem: any) => stageItem?.stageNameKebabCase == resp?.stageName);
+      this.initiativesSvc.initiative.stageName = currentStage?.stageNameKebabCase != 'pre-concept' ? 'proposal' : currentStage.stageNameKebabCase;
+      this.initiativesSvc.initiative.exactStageName = currentStage?.stageNameKebabCase;
       this.initiativesSvc.initiative.id = resp['id'];
 
       this.initiativesSvc.initiative.stageId = currentStage?.id;
@@ -146,57 +103,39 @@ export class StagesMenuComponent implements OnInit {
       const promise2 = this.initiativesSvc.getUsersByInitiative(resp['id']);
 
       forkJoin([promise1, promise2]).subscribe(
-        (val) => {
-          // console.log(val)
+        val => {
           const sucP1 = val[0];
           const sucP2 = val[1].response.users;
+          this.initiativesSvc.initiative.type = sucP1?.type;
           this.initiativesSvc.initiative.name = sucP1.name;
           this.initiativesSvc.initiative.official_code = sucP1.official_code;
           this.initiativesSvc.setTWKAttributes();
-          // this.initiativesSvc.initiative.stageId = sucP1.stages.find(stg => stg.initvStgId == sucP1.initvStgId).stageId;
-          // this.initiativesSvc.initiative.stageName = this.initiativesSvc.initiative.stageId == 2 ? 'pre-concept' : 'proposal' ;
-
           this.initiativesSvc.initiative.users = sucP2;
           this.initiativesSvc.initiative.status = sucP1.status;
           this.initiativesSvc.setTWKAttributes();
           this.getInitiativeReadOnlyValidation(this.initiativesSvc.initiative);
-          this.initiativesSvc
-            .getSubmission(
-              this.initiativesSvc.initiative.id,
-              this.initiativesSvc.initiative.stageId
-            )
-            .subscribe(
-              (res) => {
-                const sub = res.response.submission;
-                if (sub) {
-                  this.initiativesSvc.initiative.submission = sub;
-                  this.toolTipText = `Submitted by: 
+          this.initiativesSvc.getSubmission(this.initiativesSvc.initiative.id, this.initiativesSvc.initiative.stageId).subscribe(
+            res => {
+              const sub = res.response.submission;
+              if (sub) {
+                this.initiativesSvc.initiative.submission = sub;
+                this.toolTipText = `Submitted by:
               ${sub.first_name} ${sub.last_name}
-              On: 
-              ${this.datepipe.transform(
-                sub.updated_at,
-                'yyyy-MM-dd HH:mm zz'
-              )}`;
-                }
-              },
-              (error) => {
-                console.log(error);
+              On:
+              ${this.datepipe.transform(sub.updated_at, 'yyyy-MM-dd HH:mm zz')}`;
               }
-            );
+            },
+            error => {
+              console.log(error);
+            }
+          );
         },
-        (reason) => {
+        reason => {
           console.log(reason);
         }
       );
     });
   }
-
-  // managerAccesible() {
-  //   const userLeadColead = this.initiativesSvc.initiative.users.find(usr => usr.userId == this.user.id);
-  //   if (this.user.roles.find(r => r.acronym === 'ADM')) return true;
-  //   if (userLeadColead == null) return false;
-  //   return !this.initiativesSvc.initiative.readonly;
-  // }
 
   toggleMenu(menu: HTMLElement) {
     menu.classList.toggle('showMenu');
@@ -207,44 +146,24 @@ export class StagesMenuComponent implements OnInit {
   }
 
   getInitiativeReadOnlyValidation(initiative) {
-    /**
-     * Validate by roles
-     */
-    // console.log(initiative?.status)
-    this.initiativesSvc
-      .getRolefromInitiativeById(this.initiativesSvc.initiative.id)
-      .subscribe(
-        (resp) => {
-          //  console.log(resp)
-          // console.log(resp?.response?.roles[0]?.name)
-          this.initiativesSvc.initiative.userRoleName =
-            resp?.response?.roles[0]?.name;
-          this.initiativesSvc.initiative.userRoleId =
-            resp?.response?.roles[0]?.roleId;
-          this.roleIdLoaded = true;
-          //  console.log(this.initiativesSvc.initiative.userRoleName)
-          // this.initiativesSvc.setTWKAttributes();
-          let validations = () => {
-            // console.log(resp?.response?.roles[0]?.roleId)
-            //! close all platform
-            //! return true;
-            if (this.user?.roles[0].id === 1) return false;
-            if (
-              resp?.response?.roles[0]?.roleId !== 4 &&
-              resp?.response?.roles[0]?.roleId != undefined &&
-              initiative?.status == 'Editing'
-            )
-              return false;
-            return true;
-          };
-          this.initiativesSvc.initiative.readonly = validations();
-          // console.log(this.initiativesSvc.initiative.readonly)
-          // console.log(resp?.response?.roles[0]?.roleId)
-        },
-        (err) => {
-          this.roleIdLoaded = true;
-        }
-      );
+    this.initiativesSvc.getRolefromInitiativeById(this.initiativesSvc.initiative.id).subscribe(
+      resp => {
+        this.initiativesSvc.initiative.userRoleName = resp?.response?.roles[0]?.name;
+        this.initiativesSvc.initiative.userRoleId = resp?.response?.roles[0]?.roleId;
+        this.roleIdLoaded = true;
+        let validations = () => {
+          //! close all platform
+          //! return true;
+          if (this.user?.roles[0].id === 1) return false;
+          if (resp?.response?.roles[0]?.roleId !== 4 && resp?.response?.roles[0]?.roleId != undefined && initiative?.status == 'Editing') return false;
+          return true;
+        };
+        this.initiativesSvc.initiative.readonly = validations();
+      },
+      err => {
+        this.roleIdLoaded = true;
+      }
+    );
   }
 
   ngOnDestroy(): void {
@@ -254,59 +173,37 @@ export class StagesMenuComponent implements OnInit {
   }
 
   validateAllSections() {
-    // console.log('stageId ',this.initiativesSvc.initiative.stageId)
-    // console.log('id ',this.initiativesSvc.initiative.id)
-    this.initiativesSvc
-      .getSectionsValidation(
-        this.initiativesSvc.initiative.id,
-        this.initiativesSvc.initiative.stageId
-      )
-      .subscribe((resp) => {
-        if (this.initiativesSvc.initiative.stageId == 4)
-          this._dataControlService.isdcFeedbackValidation =
-            resp?.response.isdcFeedBack;
-        if (resp?.response.melia.subSections) {
-          resp?.response.melia.subSections.forEach((element) => {
-            if (element.subseDescripton === 'melia-studies-and-activities') {
-              this._dataControlService.meliaStudiesValidation = element;
-            }
-          });
-        }
-        // console.log(resp)
-        if (!resp?.response) return;
-        Object.keys(resp?.response).map((key) => {
-          let stageId = this.initiativesSvc.initiative.stageId;
-          if (!resp.response[key] || resp.response[key] == null) return;
-          let sectionId = resp.response[key]?.sectionId;
-          let ValidateGI = resp.response[key]?.validation;
-          let result = this._dataControlService?.userMenu
-            .find((item) => item.stageId == stageId)
-            ?.sections.find((item) => item.sectionId == sectionId);
-          result.fieldsCompleted = ValidateGI;
-          let subSectionsToMap = resp.response[key].subSections;
-          if (!subSectionsToMap) return;
+    this.initiativesSvc.getSectionsValidation(this.initiativesSvc.initiative.id, this.initiativesSvc.initiative.stageId).subscribe(resp => {
+      if (this.initiativesSvc.initiative.stageId == 4) this._dataControlService.isdcFeedbackValidation = resp?.response.isdcFeedBack;
+      if (resp?.response.melia.subSections) {
+        resp?.response.melia.subSections.forEach(element => {
+          if (element.subseDescripton === 'melia-studies-and-activities') {
+            this._dataControlService.meliaStudiesValidation = element;
+          }
+        });
+      }
+      if (!resp?.response) return;
+      Object.keys(resp?.response).map(key => {
+        let stageId = this.initiativesSvc.initiative.stageId;
+        if (!resp.response[key] || resp.response[key] == null) return;
+        let sectionId = resp.response[key]?.sectionId;
+        let ValidateGI = resp.response[key]?.validation;
+        let result = this._dataControlService?.userMenu.find(item => item.stageId == stageId)?.sections.find(item => item.sectionId == sectionId);
+        result.fieldsCompleted = ValidateGI;
+        let subSectionsToMap = resp.response[key].subSections;
+        if (!subSectionsToMap) return;
 
-          subSectionsToMap.map((item) => {
-            let menuSubsections = result.subsections.find(
-              (subSeItem) => subSeItem.subSectionId == item.subSectionId
-            );
-            if (menuSubsections)
-              menuSubsections.fieldsCompleted = item.validation;
+        subSectionsToMap.map(item => {
+          let menuSubsections = result.subsections.find(subSeItem => subSeItem.subSectionId == item.subSectionId);
+          if (menuSubsections) menuSubsections.fieldsCompleted = item.validation;
 
-            if (!item.hasOwnProperty('dinamicList')) return;
-            item.dinamicList?.map((resp) => {
-              if (
-                !menuSubsections?.dynamicList?.find(
-                  (dynamicItem) => dynamicItem.id == resp.impact_area_id
-                )
-              )
-                return false;
-              menuSubsections.dynamicList.find(
-                (dynamicItem) => dynamicItem.id == resp.impact_area_id
-              ).fieldsCompleted = resp.validation;
-            });
+          if (!item.hasOwnProperty('dinamicList')) return;
+          item.dinamicList?.map(resp => {
+            if (!menuSubsections?.dynamicList?.find(dynamicItem => dynamicItem.id == resp.impact_area_id)) return false;
+            menuSubsections.dynamicList.find(dynamicItem => dynamicItem.id == resp.impact_area_id).fieldsCompleted = resp.validation;
           });
         });
       });
+    });
   }
 }
